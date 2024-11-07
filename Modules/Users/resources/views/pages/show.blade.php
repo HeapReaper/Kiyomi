ce086d55d2fcc6d7990fdc91caad321c56e782a8
AutiCodes <prive@auticodes.nl>
2024-11-07 16:31:25 +0100

    Added strong tags

--- Modules/Users/resources/views/pages/show.blade.php
+++ Modules/Users/resources/views/pages/show.blade.php
@@ -8,13 +8,13 @@
       <div class="col-sm">
         <div class="pt-2 pb-2 pl-2 pr-2 mb-2 mt-2">
           <div class="form-group">
-            <label for="name" class="text-white font-weight-bold">Volledige naam</label>
+            <label for="name" class="text-white font-weight-bold"><strong>Volledige naam</strong></label>
             <input type="text" class="form-control" id="name" name="name" aria-describedby="fullname" placeholder="Voornaam achternaam" required value="{{ $user->name }}" disabled>
             <!-- <small id="fullname" class="form-text text-muted"></small>-->
           </div>
 
           <div class="form-group">
-            <label for="birthdate"  class="text-white font-weight-bold">Geboortedatum</label>
+            <label for="birthdate"  class="text-white font-weight-bold"><strong>Geboortedatum</strong></label>
             <input type="text" class="form-control" id="birthdate" name="birthdate" placeholder="01-01-2024" required value="{{ $user->birthdate }}" disabled>
           </div>
         </div>
@@ -23,17 +23,17 @@
       <div class="col-sm">
         <div class="pt-2 pb-2 pl-2 pr-2 mb-2 mt-2 rounded">
           <div class="form-group">
-            <label for="address"  class="text-white font-weight-bold">Adres</label>
+            <label for="address"  class="text-white font-weight-bold"><strong>Adres</strong></label>
             <input type="text" class="form-control" id="address" name="address" placeholder="Straatnaam nummer" required value="{{ $user->address }}" disabled>
           </div>
 
           <div class="form-group">
-            <label for="postcode"  class="text-white font-weight-bold">Postcode</label>
+            <label for="postcode"  class="text-white font-weight-bold"><strong>Postcode</strong></label>
             <input type="text" class="form-control" id="postcode" name="postcode" placeholder="1234AH (zonder spatie!)" required value="{{ $user->zip_code }}" disabled>
           </div>
 
           <div class="form-group">
-            <label for="city"  class="text-white font-weight-bold">Woonplaats</label>
+            <label for="city"  class="text-white font-weight-bold"><strong>Woonplaats</strong></label>
             <input type="text" class="form-control" id="city" name="city" placeholder="Woonplaats" required value="{{ $user->city }}" disabled>
           </div>
         </div>
@@ -42,12 +42,12 @@
       <div class="col-sm">
         <div class="pt-2 pb-2 pl-2 pr-2 mb-2 mt-2 rounded">
           <div class="form-group">
-            <label for="phone"  class="text-white font-weight-bold">Telefoon</label>
+            <label for="phone"  class="text-white font-weight-bold"><strong>Telefoon</strong></label>
             <input type="text" class="form-control" id="phone" name="phone" placeholder="0612343455 (nummer zonder streepje!)" required value="{{ $user->mobile_phone }}" disabled>
           </div>
 
           <div class="form-group">
-            <label for="email" class="text-white font-weight-bold">E-mail</label>
+            <label for="email" class="text-white font-weight-bold"><strong>E-mail</strong></label>
             <input type="email" class="form-control" id="email" name="email" placeholder="mail@provider.nl" required value="{{ $user->email }}" disabled>
           </div>
         </div>
@@ -58,12 +58,12 @@
       <div class="col-sm">
         <div class="pt-2 pb-2 pl-2 pr-2 mb-2 mt-2">
           <div class="form-group">
-            <label for="rdw_number"  class="text-white font-weight-bold">RDW nummer</label>
+            <label for="rdw_number"  class="text-white font-weight-bold"><strong>RDW nummer</strong></label>
             <input type="text" class="form-control" id="rdw_number" name="rdw_number" placeholder="A34hjhdwqjkwqa" value="{{ $user->rdw_number }}" disabled>
           </div>
 
           <div class="form-group">
-            <label for="knvvl"  class="text-white font-weight-bold">KNVvl nummer</label>
+            <label for="knvvl"  class="text-white font-weight-bold"><strong>KNVvl nummer</strong></label>
             <input type="text" class="form-control" id="knvvl" name="knvvl" placeholder="1234567" value="{{ $user->KNVvl }}" disabled>
           </div>
         </div>
@@ -72,7 +72,7 @@
       <div class="col-sm">
         <div class="pt-2 pb-2 pl-2 pr-2 mb-2 mt-2">
           <div class="form-group">
-            <label for="role" class="text-white font-weight-bold">Rol</label>
+            <label for="role" class="text-white font-weight-bold"><strong>Rol</strong></label>
             <select class="form-control" id="role" name="role" required disabled>
               <option value="junior_member" @if ($user->hasRole('junior_member')) selected @endif>
                 Jeugd lid
@@ -99,7 +99,7 @@
           </div> 
 
           <div class="form-group">
-            <label for="instruct" class="text-white font-weight-bold">Instructeur</label>
+            <label for="instruct" class="text-white font-weight-bold"><strong>Instructeur</strong></label>
             <select class="form-control" id="instruct" name="instruct" required disabled>
               <option value=0 @if ($user->instruct == 0) selected @endif>
                 Nee
@@ -114,7 +114,7 @@
 
       <div class="col-sm">
         <div class="pt-2 pb-2 pl-2 pr-2 mb-2 mt-2">
-          <p class="font-weight-bold text-white mb-0">Brevetten</p>
+          <p class="font-weight-bold text-white mb-0"><strong>Brevetten</strong></p>
           <div class="form-check">
             <input class="form-check-input" type="checkbox" value=1 id="PlaneCertCheckbox" name="PlaneCertCheckbox"  @if ($user->has_plane_brevet == 1) checked @endif disabled>
             <label class="form-check-label text-white" for="PlaneCertCheckbox">
@@ -136,7 +136,7 @@
             </label>
           </div>
 
-          <p class="font-weight-bold text-white mb-0 mt-3">Erelid</p>
+          <p class="font-weight-bold text-white mb-0 mt-3"><strong>Erelid</strong></p>
           <div class="form-check">
             <input class="form-check-input" type="checkbox" value=1 id="honoraryMemberCheckbox" name="honoraryMemberCheckbox" @if ($user->in_memoriam == 1) checked @endif disabled>
             <label class="form-check-label text-white" for="honoraryMemberCheckbox">
