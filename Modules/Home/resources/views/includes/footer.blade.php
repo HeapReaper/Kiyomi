2d4cb65f4e82d7fd2b6122ad218c1bceea4c23b0
AutiCodes <prive@auticodes.nl>
2024-11-07 13:44:52 +0100

    Made home UI pwetty

--- Modules/Home/resources/views/includes/footer.blade.php
+++ Modules/Home/resources/views/includes/footer.blade.php
@@ -1,5 +1,5 @@
-<footer class="footer text-center mt-4">
-  <p class="text-white">
+<footer class="footer text-center mt-4 mb-0 bg-dark bg-opacity-50" style="bottom: 0px; position:fixed; left:0px; bottom:0px; width: 100%">
+  <p class="text-white mt-1">
     Copyright &copy; 
     <a href="http://trmc.nl">
       TRMC
