4c9df604c7fc4dad184f6ae3dcfe50f1951103f7
AutiCodes <prive@auticodes.nl>
2024-11-09 13:17:32 +0100

    Working users contact page

--- Modules/Users/app/Http/Controllers/UsersContactController.php
+++ Modules/Users/app/Http/Controllers/UsersContactController.php
@@ -4,11 +4,17 @@ namespace Modules\Users\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Modules\Users\Events\UsersContactWasSubmitted;
+use Mail;
+use Modules\Users\Models\User;
 
 class UsersContactController extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     * @author AutiCodes
+     * @return View
      */
     public function index()
     {
@@ -24,11 +30,42 @@ class UsersContactController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Store a newly created resource in storage and sends email
+     *
+     * @author AutiCodes
+     * @param Request $request
+     * @return RedirectResponse
      */
     public function store(Request $request)
     {
-        //
+        // TODO: Better validation
+        $validated = $request->validate([
+            'send_to' => ['required'],
+            'subject' => ['required'],
+            'content' => ['required'],
+        ]);
+
+        $usersToSend = User::with('roles')->whereHas('roles', function($query) use ($validated) {
+            $query->whereIn('name', $validated['send_to']);
+        })->get();
+
+        // TODO, sthis is cringe..
+        $usersEmails = [];
+        foreach ($usersToSend as $user) {
+            array_push($usersEmails, "$user->email");
+        }
+
+        try {
+            Mail::html($validated['content'], function ($message) use ($validated, $usersEmails) {
+                $message->bcc($usersEmails)
+                        ->subject($validated['subject']);
+            });
+        } catch (Exception $e) {
+            // Add decent error handling
+            return redirect()->back()->with('error', 'Er ging iets mis! Error: ' . $e->getMessage());
+        }
+
+        return redirect()->back()->with('success', 'Email is verstuurd!');
     }
 
     /**
--- Modules/Users/resources/views/pages/contact_index.blade.php
+++ Modules/Users/resources/views/pages/contact_index.blade.php
@@ -8,50 +8,50 @@
     <form action="{{ route('contact.store') }}", method="POST">
       @csrf
       <div class="form-check m-1">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" style="width: 20px; height: 20px">
+        <input class="form-check-input" type="checkbox" value="junior_member" id="junior_members" name="send_to[]" style="width: 20px; height: 20px">
         <label class="form-check-label text-white" for="flexCheckDefault">
           <strong>Jeugd leden</strong>
         </label>
       </div>
 
       <div class="form-check m-1">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" style="width: 20px; height: 20px">
-        <label class="form-check-label text-white" for="flexCheckDefault">
+        <input class="form-check-input" type="checkbox" value="aspirant_member" id="aspirant_member" name="send_to[]" style="width: 20px; height: 20px">
+        <label class="form-check-label text-white" for="aspirant_member">
           <strong>Aspirant leden</strong>
         </label>
       </div>
 
       <div class="form-check m-1">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" style="width: 20px; height: 20px">
-        <label class="form-check-label text-white" for="flexCheckDefault">
+        <input class="form-check-input" type="checkbox" value="member" id="member" name="send_to[]" style="width: 20px; height: 20px">
+        <label class="form-check-label text-white" for="member">
           <strong>Leden</strong>
         </label>
       </div>
 
       <div class="form-check m-1">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" style="width: 20px; height: 20px">
-        <label class="form-check-label text-white" for="flexCheckDefault">
+        <input class="form-check-input" type="checkbox" value="management" id="management" name="send_to[]" style="width: 20px; height: 20px">
+        <label class="form-check-label text-white" for="management">
           <strong>Bestuurs leden</strong>
         </label>
       </div>
 
       <div class="form-check m-1">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" style="width: 20px; height: 20px">
-        <label class="form-check-label text-white" for="flexCheckDefault">
+        <input class="form-check-input" type="checkbox" value="donor" id="donor" name="send_to[]" style="width: 20px; height: 20px">
+        <label class="form-check-label text-white" for="donor">
           <strong>Donateurs</strong>
         </label>
       </div>
 
       <div class="form-check m-1">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" style="width: 20px; height: 20px">
-        <label class="form-check-label text-white" for="flexCheckDefault">
+        <input class="form-check-input" type="checkbox" value="not_paid" id="not_paid" name="send_to[]" style="width: 20px; height: 20px">
+        <label class="form-check-label text-white" for="not_paid">
           <strong>Niet betaalden</strong>
         </label>
       </div>
 
       <div class="form-group m-1">
         <h4 class="text-white mt-2">Onderwerp</h4>
-        <input type="text" class="form-control" id="subject" aria-describedby="subject" placeholder="Onderwerp...">
+        <input type="text" class="form-control" id="subject" name="subject" aria-describedby="subject" placeholder="Onderwerp...">
       </div>
 
       <script src="js/tinymce/tinymce.min.js" referrerpolicy="origin"></script>
@@ -67,13 +67,12 @@
             height: 600,
           });
         </script>
-        <textarea name="text_editor">
+        <textarea name="content">
           <!-- Text -->
         </textarea>
       </div>
 
       <button type="submit" class="btn text-white m-1" style="background-image: linear-gradient(45deg, #874da2 0%, #c43a30 100%)">Verstuur</button>
-
     </form>
   </div>
 @endsection
