c83b1ce8522651d8b1df857ce27461d0742295cd
AutiCodes <prive@auticodes.nl>
2024-11-14 18:14:54 +0100

    Changed margins

--- Modules/Panel/resources/views/layouts/master.blade.php
+++ Modules/Panel/resources/views/layouts/master.blade.php
@@ -1,122 +1,124 @@
 <!DOCTYPE html>
 <html lang="nl">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" >
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1" >
-  <meta name="csrf-token" content="{{ csrf_token() }}">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>@yield('title')</title>
 
-  <title>@yield('title')</title>
+    <meta name="description" content="{{ $description ?? '' }}">
+    <meta name="keywords" content="{{ $keywords ?? '' }}">
+    <meta name="author" content="{{ $author ?? '' }}">
 
-  <meta name="description" content="{{ $description ?? '' }}">
-  <meta name="keywords" content="{{ $keywords ?? '' }}">
-  <meta name="author" content="{{ $author ?? '' }}">
+    @include('panel::includes.head')
+    <!-- Favicon -->
+    <link rel="icon" href="/app_media/faticon.ico" type="image/x-icon">
+    <livewire:styles />
+    @livewireStyles
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
-  @include('panel::includes.head')
-  <!-- Favicon -->
-  <link rel="icon" href="/app_media/faticon.ico" type="image/x-icon">
-  <livewire:styles />
-  @livewireStyles
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    {{-- Vite CSS --}}
+    {{-- {{ module_vite('build-admin', 'resources/assets/sass/app.scss') }} --}}
+  </head>
 
-  {{-- Vite CSS --}}
-  {{-- {{ module_vite('build-admin', 'resources/assets/sass/app.scss') }} --}}
-</head>
+  <body>
+    <main>
+      @include('panel::includes.navbar')
 
-<body>
-  @include('panel::includes.navbar')
-
-  <!-- Errors -->
-  @if ($errors->any())
-    <div class="toast-container showposition-fixed bottom-0 end-0 p-3">
-      <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-        <div class="toast-header">
-          <img src="/app_media/Kiyomi_logo.png" class="rounded me-2" alt="..." style="max-width: 35px">
-          <strong class="me-auto">Fout!</strong>
-          <small>Een paar seconden geleden</small>
-          <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+      <!-- Errors -->
+      @if ($errors->any())
+        <div class="toast-container showposition-fixed bottom-0 end-0 p-3">
+          <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="toast-header">
+              <img src="/app_media/Kiyomi_logo.png" class="rounded me-2" alt="..." style="max-width: 35px">
+              <strong class="me-auto">Fout!</strong>
+              <small>Een paar seconden geleden</small>
+              <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+            <div class="toast-body">
+              @foreach ($errors->all() as $error)
+              <li>{{ session('error') }}</li>
+              @endforeach
+            </div>
+          </div>
         </div>
-        <div class="toast-body">
-          @foreach ($errors->all() as $error)
-          <li>{{ session('error') }}</li>
-          @endforeach
+
+        <script>
+          const toastLiveExample = document.getElementById('liveToast')
+          const toast = new bootstrap.Toast(toastLiveExample)
+          toast.show()
+        </script>
+      @endif
+
+      @if (session()->has('error'))
+        <div class="toast-container showposition-fixed bottom-0 end-0 p-3">
+          <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="toast-header">
+              <img src="/app_media/Kiyomi_logo.png" class="rounded me-2" alt="..." style="max-width: 35px">
+              <strong class="me-auto">Fout!</strong>
+              <small>Een paar seconden geleden</small>
+              <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+            <div class="toast-body">
+              {{ session('error') }}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
 
-    <script>
-      const toastLiveExample = document.getElementById('liveToast')
-      const toast = new bootstrap.Toast(toastLiveExample)
-      toast.show()
-    </script>
-  @endif
+        <script>
+          const toastLiveExample = document.getElementById('liveToast')
+          const toast = new bootstrap.Toast(toastLiveExample)
+          toast.show()
+        </script>
+      @endif
 
-  @if (session()->has('error'))
-    <div class="toast-container showposition-fixed bottom-0 end-0 p-3">
-      <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-        <div class="toast-header">
-          <img src="/app_media/Kiyomi_logo.png" class="rounded me-2" alt="..." style="max-width: 35px">
-          <strong class="me-auto">Fout!</strong>
-          <small>Een paar seconden geleden</small>
-          <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+      <!-- Success -->
+      @if (session()->has('success'))
+        <div class="toast-container showposition-fixed bottom-0 end-0 p-3">
+          <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="toast-header">
+              <img src="/app_media/Kiyomi_logo.png" class="rounded me-2" alt="..." style="max-width: 35px">
+              <strong class="me-auto">Success!</strong>
+              <small>Een paar seconden geleden</small>
+              <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+            <div class="toast-body">
+              {{ session('success') }}
+            </div>
+          </div>
         </div>
-        <div class="toast-body">
-          {{ session('error') }}
-        </div>
-      </div>
-    </div>
 
-    <script>
-      const toastLiveExample = document.getElementById('liveToast')
-      const toast = new bootstrap.Toast(toastLiveExample)
-      toast.show()
-    </script>
-  @endif
+        <script>
+          const toastLiveExample = document.getElementById('liveToast')
+          const toast = new bootstrap.Toast(toastLiveExample)
+          toast.show()
+        </script>
+      @endif
 
-  <!-- Success -->
-  @if (session()->has('success'))
-    <div class="toast-container showposition-fixed bottom-0 end-0 p-3">
-      <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-        <div class="toast-header">
-          <img src="/app_media/Kiyomi_logo.png" class="rounded me-2" alt="..." style="max-width: 35px">
-          <strong class="me-auto">Success!</strong>
-          <small>Een paar seconden geleden</small>
-          <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-        </div>
-        <div class="toast-body">
-          {{ session('success') }}
-        </div>
-      </div>
-    </div>
+      @yield('content')
 
-    <script>
-      const toastLiveExample = document.getElementById('liveToast')
-      const toast = new bootstrap.Toast(toastLiveExample)
-      toast.show()
-    </script>
-  @endif
+      <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+      <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
 
-  @yield('content')
+      {{-- Vite JS --}}
+      {{-- {{ module_vite('build-admin', 'resources/assets/js/app.js') }} --}}
 
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+      <style>
+        body, html {
+          background-image: linear-gradient(45deg, #874da2 0%, #c43a30 100%);
+          width: 100%;
+          height: 100%;
+          background-attachment: fixed;
+        }
 
-  {{-- Vite JS --}}
-  {{-- {{ module_vite('build-admin', 'resources/assets/js/app.js') }} --}}
-
-  <style>
-    body, html {
-      background-image: linear-gradient(45deg, #874da2 0%, #c43a30 100%);
-      width: 100%;
-      height: 100%;
-      background-attachment: fixed;
-    }
-
-  </style>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-  <livewire:scripts />
-  @livewireScripts
-</body>
+      </style>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+      <livewire:scripts />
+      @livewireScripts
+    </main>
+  </body>
+</html>
--- Modules/Panel/resources/views/pages/index.blade.php
+++ Modules/Panel/resources/views/pages/index.blade.php
@@ -4,53 +4,55 @@
 
 @section('content')
   <!-- Total container-->
-  <div class="container">
-    <h1 class="text-white mt-4">Admin home</h1>
+  <div class="ms-0">
+    <div class="container">
+      <h1 class="text-white mt-4">Admin home</h1>
 
-    <!-- Total -->
-    <h3 class="text-white mt-4">Vluchten overzicht</h3>
-    <!-- Total cards -->
-    <div class="row">
-      <!-- Total flights -->
-      <div class="col-sm text-center ml-2 mr-2 mt-2 bg-dark bg-opacity-25 rounded shadow-lg  m-2">
-        <h3 class="text-white mt-2">Totaal:</h3>
-        <h1 class="text-white">0</h1>
+      <!-- Total -->
+      <h3 class="text-white mt-4">Vluchten overzicht</h3>
+      <!-- Total cards -->
+      <div class="row">
+        <!-- Total flights -->
+        <div class="col-sm text-center ml-2 mr-2 mt-2 bg-dark bg-opacity-25 rounded shadow-lg  m-2">
+          <h3 class="text-white mt-2">Totaal:</h3>
+          <h1 class="text-white">0</h1>
+        </div>
+        <!-- This week flights -->
+        <div class="col-sm text-center ml-2 mr-2 mt-2 bg-dark bg-opacity-25 rounded shadow-lg m-2">
+          <h3 class="text-white mt-2">Deze week:</h3>
+          <h1 class="text-white">0</h1>
+        </div>
+        <!-- Today flights -->
+        <div class="col-sm text-center ml-2 mr-2 mt-2 bg-dark bg-opacity-25 rounded shadow-lg m-2">
+          <h3 class="text-white mt-2">Vandaag:</h3>
+          <h1 class="text-white">0</h1>
+        </div>
       </div>
-      <!-- This week flights -->
-      <div class="col-sm text-center ml-2 mr-2 mt-2 bg-dark bg-opacity-25 rounded shadow-lg m-2">
-        <h3 class="text-white mt-2">Deze week:</h3>
-        <h1 class="text-white">0</h1>
-      </div>
-      <!-- Today flights -->
-      <div class="col-sm text-center ml-2 mr-2 mt-2 bg-dark bg-opacity-25 rounded shadow-lg m-2">
-        <h3 class="text-white mt-2">Vandaag:</h3>
-        <h1 class="text-white">0</h1>
-      </div>
+      <!-- End total cards -->
     </div>
-    <!-- End total cards -->
-  </div>
 
-  <div class="container">
-    <!-- Total -->
-    <h3 class="text-white mt-4">Leden overzicht</h3>
-    <!-- Total cards -->
-    <div class="row">
-      <!-- Total flights -->
-      <div class="col-sm text-center ml-2 mr-2 mt-2 bg-dark bg-opacity-25 rounded shadow-lg m-2">
-        <h3 class="text-white mt-2">Totaal ingeschreven:</h3>
-        <h1 class="text-white">0</h1>
+    <div class="container">
+      <!-- Total -->
+      <h3 class="text-white mt-4">Leden overzicht</h3>
+      <!-- Total cards -->
+      <div class="row">
+        <!-- Total flights -->
+        <div class="col-sm text-center ml-2 mr-2 mt-2 bg-dark bg-opacity-25 rounded shadow-lg m-2">
+          <h3 class="text-white mt-2">Totaal ingeschreven:</h3>
+          <h1 class="text-white">0</h1>
+        </div>
+        <!-- This week flights -->
+        <div class="col-sm text-center ml-2 mr-2 mt-2 bg-dark bg-opacity-25 rounded shadow-lg m-2">
+          <h3 class="text-white mt-2">Leden</h3>
+          <h1 class="text-white">0</h1>
+        </div>
+        <!-- Today flights -->
+        <div class="col-sm text-center ml-2 mr-2 mt-2 bg-dark bg-opacity-25 rounded shadow-lg m-2">
+          <h3 class="text-white mt-2">Donateurs:</h3>
+          <h1 class="text-white">0</h1>
+        </div>
       </div>
-      <!-- This week flights -->
-      <div class="col-sm text-center ml-2 mr-2 mt-2 bg-dark bg-opacity-25 rounded shadow-lg m-2">
-        <h3 class="text-white mt-2">Leden</h3>
-        <h1 class="text-white">0</h1>
-      </div>
-      <!-- Today flights -->
-      <div class="col-sm text-center ml-2 mr-2 mt-2 bg-dark bg-opacity-25 rounded shadow-lg m-2">
-        <h3 class="text-white mt-2">Donateurs:</h3>
-        <h1 class="text-white">0</h1>
-      </div>
+      <!-- End total cards -->
     </div>
-    <!-- End total cards -->
   </div>
 @stop
