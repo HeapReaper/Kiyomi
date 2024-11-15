ea4dce73d70f0653d56a2a0005ec553a1d2e1bda
AutiCodes <prive@auticodes.nl>
2024-11-15 10:38:51 +0100

    Removed footer

--- Modules/Panel/resources/views/includes/footer.blade.php
+++ Modules/Panel/resources/views/includes/footer.blade.php
@@ -1,3 +1,3 @@
-<footer class="p-2 my-4 bg-dark bg-opacity-25">
+<footer class="p-2 my-4 bg-dark bg-opacity-25 sticky-footer">
   <p class="text-center text-white text-body-secondary">© {{ date('Y') }} Kiyomi</p>
 </footer>
