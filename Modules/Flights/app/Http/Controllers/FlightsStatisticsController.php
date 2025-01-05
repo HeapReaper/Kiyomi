c5b87ea64acff5962e915c686514f1fadda6088c
AutiCodes <prive@auticodes.nl>
2025-01-05 21:28:22 +0100

    Added chart.js

--- Modules/Flights/app/Http/Controllers/FlightsStatisticsController.php
+++ Modules/Flights/app/Http/Controllers/FlightsStatisticsController.php
@@ -9,7 +9,7 @@ class FlightsStatisticsController extends Controller
 {
     public function index()
     {
-        return view('flights::index');
+        return view('flights::pages.statistics');
     }
 
     public function create()
---
+++ Modules/Flights/resources/views/pages/statistics.blade.php
@@ -0,0 +1,9 @@
+@extends('panel::layouts.master')
+
+@section('title', 'Statistieken')
+
+@section('content')
+  <div class="m-2">
+    <h1>Hoi</h1>
+  </div>
+@stop
