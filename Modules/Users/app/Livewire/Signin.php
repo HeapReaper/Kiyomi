66c4627b4826583ad62a475a1e89d52c7b7a48ea
AutiCodes <prive@auticodes.nl>
2025-01-05 20:11:45 +0100

    Added livewire files for signIn

---
+++ Modules/Users/app/Livewire/Signin.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\Users\Livewire;
+
+use Livewire\Component;
+
+class Signin extends Component
+{
+    public function render()
+    {
+        return view('users::livewire.signin');
+    }
+}
