fa7c5c45a7910ad24986e504d3e451d40017f634
AutiCodes <prive@auticodes.nl>
2025-01-06 20:44:48 +0100

    Added flights and pilots charts

---
+++ Modules/Flights/app/Livewire/Statistics.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Modules\Flights\Livewire;
+
+use Livewire\Component;
+use App\Helpers\Statistics as StatisticsHelper;
+use App\Helpers\createChart;
+
+class Statistics extends Component
+{
+	public $selectYear;
+
+    public function mount()
+    {
+        $this->selectYear = now()->year;
+    }
+
+    public function updateSelectYear()
+    {
+        $this->selectYear = $this->selectYear;
+    }
+	
+    public function render()
+    {
+		$flightsThisYearCount = createChart::createColumnChart(
+			"Aantal vluchten per maand",
+			StatisticsHelper::getFlightCountEachMonth($this->selectYear), // data
+			false,
+			false
+		);
+		
+		$TopTenPilots = createChart::createLineChart(
+			'Top 10 piloten met meeste vluchten',
+			StatisticsHelper::getTopTenMostFrequentFlyers($this->selectYear),
+			false,
+			true
+		);
+		
+//		$modelFlightsCount = createChart::createColumnChart(
+//			"Aantal vluchten per model",
+//
+//		);
+		
+		return view('flights::livewire.statistics', [
+			'flightsThisYearCount' => $flightsThisYearCount,
+			'topTenPilots' => $TopTenPilots,
+		]);
+    }
+}
