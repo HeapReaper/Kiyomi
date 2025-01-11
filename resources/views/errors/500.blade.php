b86b4b4364b9e8dfb2afdb788075e5c4e3d400e1
AutiCodes <prive@auticodes.nl>
2025-01-11 14:42:26 +0100

    Added error 500 page

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,12 +1,12 @@
 @extends('errors.layout')
 
-@section('title', '404')
+@section('title', '500')
 
 @section('content')
   <div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
     <div class="text-center">
-      <img src="/app_media/plane-404.png">
-      <p>Oeps! Ik heb deze pagina niet gevonden!</p>
+      <img src="/app_media/plane-500.png">
+      <p>Oeps! Er ging iets goed mis met de server! Het probleem is gerapporteerd.</p>
     </div>
   </div>
 @stop
