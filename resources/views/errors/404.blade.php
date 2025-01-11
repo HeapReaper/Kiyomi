fc0b950e4ac460f503416e9b67873b039be841d2
AutiCodes <prive@auticodes.nl>
2025-01-11 14:29:27 +0100

    Added 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('errors.layout')
+
+@section('title', '404')
+
+@section('content')
+  <div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
+    <div class="text-center">
+      <img src="/app_media/plane-404.png">
+      <p>Oeps! Ik heb deze pagina niet gevonden!</p>
+    </div>
+  </div>
+
+@stop
