99c8691f80d0dbffc645400d2b03f090ff63463a
AutiCodes <prive@auticodes.nl>
2025-01-15 15:34:12 +0100

    Added migration and financial module

---
+++ Modules/Financials/database/seeders/FinancialsDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Financials\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class FinancialsDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
