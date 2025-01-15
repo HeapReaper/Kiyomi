10e2b0b4700ab44f275f2d59843999e5c17c50bc
AutiCodes <prive@auticodes.nl>
2025-01-15 18:41:02 +0100

    Removed comments from financials controllers

--- Modules/Financials/app/Http/Controllers/FinancialsController.php
+++ Modules/Financials/app/Http/Controllers/FinancialsController.php
@@ -7,57 +7,36 @@ use Illuminate\Http\Request;
 
 class FinancialsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         return view('financials::financials.index');
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
+	
     public function create()
     {
         return view('financials::create');
     }
-
-    /**
-     * Store a newly created resource in storage.
-     */
+	
     public function store(Request $request)
     {
         //
     }
-
-    /**
-     * Show the specified resource.
-     */
+	
     public function show($id)
     {
         return view('financials::show');
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
+	
     public function edit($id)
     {
         return view('financials::edit');
     }
-
-    /**
-     * Update the specified resource in storage.
-     */
+	
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy($id)
     {
         //
