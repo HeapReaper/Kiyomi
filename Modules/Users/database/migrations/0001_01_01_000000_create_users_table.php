0c4b4c0a48ae92400da4e503fc5dd3fa21a93646
AutiCodes <kelvin@aerobytes.nl>
2025-01-20 13:40:06 +0100

    Added user tests and updated test.yml

--- Modules/Users/database/migrations/0001_01_01_000000_create_users_table.php
+++ Modules/Users/database/migrations/0001_01_01_000000_create_users_table.php
@@ -35,7 +35,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
-            $table->rememberToken();
+            $table->rememberToken()->nullable();
             $table->timestamps();
         });
 
---
+++ Modules/Users/tests/Feature/UsersTest.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Modules\Users\Tests\Feature;
+
+use Illuminate\Support\Facades\Artisan;
+use Modules\Users\Models\User;
+use Tests\TestCase;
+use Livewire\Livewire;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UsersTest extends TestCase
+{
+	public function setUp(): void
+	{
+		parent::setUp();
+		Artisan::call('migrate');
+		
+		Artisan::call('db:seed', ['--class' => 'DatabaseSeeder']);
+		Artisan::call('db:seed', ['--class' => 'UserRoleSeeder']);
+		
+		// sign in user for tests below
+		Livewire::test('users::signin')
+			->set('email', 'admin@default.com')
+			->set('password', 'admin')
+			->call('submit');
+		
+		$this->assertAuthenticatedAs(User::find(1));
+	}
+	
+    public function test_users_can_log_in(): void
+	{
+		$user = User::find(1);
+		
+		$this->assertNotNull($user);
+		
+		$response = Livewire::test('users::signin')
+			->set('email', 'admin@default.com')
+			->set('password', 'admin')
+			->call('submit');
+
+		$this->assertAuthenticatedAs($user);
+
+		$response->assertRedirect('/panel');
+	}
+	
+	public function test_members_can_be_created(): void
+	{
+		$response = $this->post(route('users.store'), [
+			'role' => 'member',
+			'name' => 'John Doe',
+			'birthdate' => '01-01-2000',
+			'address' => 'Test Address',
+			'postcode' => '12345',
+			'city' => 'Test City',
+			'phone' => '0123456789',
+			'email' => 'john@doe.com',
+			'instruct' => 0,
+		]);
+		
+		$this->assertDatabaseHas('users', [
+			'name' => 'John Doe',
+		]);
+		
+		$response->assertRedirect('/users');
+	}
+	
+	public function test_members_can_be_updated(): void
+	{
+		$userToEdit = (User::where('name', 'Default user')->first())->id;
+		
+		$response = $this->put(route('users.update', $userToEdit), [
+			'name' => 'John Doe 2',
+			'birthdate' => '01-01-2000',
+			'address' => 'Test Address',
+			'postcode' => '1234AA',
+			'city' => 'Test City',
+			'phone' => '0123456789',
+			'email' => 'john@doe2.com',
+			'instruct' => 0,
+			'rdw_number' => 'rfsjregre',
+			'knvvl' => 'wdeqfdwe',
+			'role' => 'member',
+		]);
+		
+		$this->assertDatabaseHas('users', [
+			'name' => 'John Doe 2',
+		]);
+		
+		$response->assertRedirect('/users');
+	}
+}
