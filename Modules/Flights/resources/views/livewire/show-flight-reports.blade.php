a1bcd674ff3efe187ab83b20205b5a055fdd4718
AeroBytes <kelvin@aerobytes.nl>
2025-02-22 20:21:52 +0100

    Added flight report delete button

--- Modules/Flights/resources/views/livewire/show-flight-reports.blade.php
+++ Modules/Flights/resources/views/livewire/show-flight-reports.blade.php
@@ -8,7 +8,7 @@
           <th scope="col">Rapport eind datum</th>
           <th scope="col">Gemaakt door</th>
           <th scope="col">Gemaakt op</th>
-          <th scope="col">Download</th>
+          <th scope="col">Opties</th>
         </tr>
       </thead>
       <tbody>
@@ -20,7 +20,14 @@
             <td>{{ $report->made_by }}</td>
             <td>{{ $report->created_at->format('d-m-Y') }}</td>
             <td>
-              <a href="flights-reports/download/{{ $report->file }}" class="btn text-white" style="background-image: linear-gradient(45deg, #874da2 0%, #c43a30 100%)">Download</a>
+              <a href="flights-reports/download/{{ $report->file }}" class="btn text-white" style="background-image: linear-gradient(45deg, #874da2 0%, #c43a30 100%)">
+                Download
+              </a>
+              <a href="flights-reports/destroy/{{ $report->id }}" class="btn text-white" style="background-image: linear-gradient(45deg, #874da2 0%, #c43a30 100%)"
+                 onclick="return confirm('Weet je zeker dat je deze vlucht rapportage wilt verwijderen?')"
+              >
+                Verwijder
+              </a>
             </td>
           </tr>
         @endforeach
