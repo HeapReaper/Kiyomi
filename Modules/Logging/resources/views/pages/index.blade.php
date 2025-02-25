8f7aa6dfdd812c104c6bb68a4f7f63a9634c552e
AeroBytes <kelvin@aerobytes.nl>
2025-02-25 16:53:26 +0100

    Added log purge frontend

--- Modules/Logging/resources/views/pages/index.blade.php
+++ Modules/Logging/resources/views/pages/index.blade.php
@@ -1,23 +1,41 @@
 @extends('panel::layouts.master')
 
+@section('title', 'Logs')
+
 @section('content')
   <div class="container bg-dark bg-opacity-25 rounded shadow-lg mt-4 p-2">
     <h2 class="text-white">Logs</h2>
 
-    <ul class="nav nav-tabs" id="log-tabs" role="tablist">
-      <li class="nav-item" role="presentation">
-        <button class="nav-link active" id="laravel-logs-tab" data-bs-toggle="tab" data-bs-target="#laravel-logs"
-                type="button" role="tab" aria-controls="home" aria-selected="true">
-          Laravel logs
-        </button>
-      </li>
-      <li class="nav-item" role="presentation">
-        <button class="nav-link" id="user-error-tab" data-bs-toggle="tab" data-bs-target="#user-errors" type="button"
-                role="tab" aria-controls="profile" aria-selected="false">
-          Gebruiker fout log
-        </button>
-      </li>
-    </ul>
+    <div class="row">
+      <div class="col">
+        <div class="float-start">
+          <ul class="nav nav-tabs" id="log-tabs" role="tablist">
+            <li class="nav-item" role="presentation">
+              <button class="nav-link active" id="laravel-logs-tab" data-bs-toggle="tab" data-bs-target="#laravel-logs"
+                      type="button" role="tab" aria-controls="home" aria-selected="true">
+                Laravel logs
+              </button>
+            </li>
+            <li class="nav-item" role="presentation">
+              <button class="nav-link" id="user-error-tab" data-bs-toggle="tab" data-bs-target="#user-errors" type="button"
+                      role="tab" aria-controls="profile" aria-selected="false">
+                Gebruiker fout log
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="col">
+        <div class="float-end">
+          <a href="/logs-purge" onclick="return confirm('Weet je zeker dat je de logs wilt wissen?')">
+            <x-heroicon-o-trash stroke="white" style="width: 27px;"/>
+          </a>
+          <script>
+          </script>
+        </div>
+      </div>
+    </div>
+
 
     <div class="tab-content bg-dark rounded mt-2 mb-1 p-2" id="tab-content">
       <div class="tab-pane fade show active" id="laravel-logs" role="tabpanel" aria-labelledby="laravel-logs">
