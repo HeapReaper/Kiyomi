9ec8c93d239f0903f1aad5f38f798a5039f9a8c4
AeroBytes <kelvin@aerobytes.nl>
2025-02-25 15:20:36 +0100

    Panel also uses local Bootstrap

--- Modules/Panel/resources/views/layouts/master.blade.php
+++ Modules/Panel/resources/views/layouts/master.blade.php
@@ -12,18 +12,20 @@
     <meta name="keywords" content="{{ $keywords ?? '' }}">
     <meta name="author" content="{{ $author ?? '' }}">
 
-    @include('panel::includes.head')
     <!-- Favicon -->
     <link rel="icon" href="/app_media/faticon.ico" type="image/x-icon">
     <livewire:styles />
     @livewireStyles
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
     <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <link href="{{ asset('bootstrap-5.3.3-dist/css/bootstrap.css') }}" rel="stylesheet">
+
     {{-- Vite CSS --}}
     {{-- {{ module_vite('build-admin', 'resources/assets/sass/app.scss') }} --}}
   </head>
@@ -127,11 +129,6 @@
 
       @yield('content')
 
-      <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-      <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
-
       {{-- Vite JS --}}
       {{-- {{ module_vite('build-admin', 'resources/assets/js/app.js') }} --}}
 
@@ -212,8 +209,8 @@
           border-color: #2b5c93;
         }
       </style>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
+      <script src="{{ asset('bootstrap-5.3.3-dist/js/bootstrap.bundle.js') }}"></script>
       <livewire:scripts />
       @livewireScripts
     </main>
