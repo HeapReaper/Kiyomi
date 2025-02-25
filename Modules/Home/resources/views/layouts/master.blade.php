3d9453a0067b402ea47de070195dd6a89a9da4a0
AeroBytes <kelvin@aerobytes.nl>
2025-02-25 15:12:42 +0100

    Moved bootstrap to local

--- Modules/Home/resources/views/layouts/master.blade.php
+++ Modules/Home/resources/views/layouts/master.blade.php
@@ -16,8 +16,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
 
-    @include('home::includes.head')
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <link href="{{ asset('bootstrap-5.3.3-dist/css/bootstrap.css') }}" rel="stylesheet">
+    <link rel="icon" href="/app_media/faticon.ico" type="image/x-icon">
 
     {{-- Vite CSS --}}
     {{-- {{ module_vite('build-home', 'resources/assets/sass/app.scss') }} --}}
@@ -110,7 +110,7 @@
       }
     </style>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="{{ asset('bootstrap-5.3.3-dist/js/bootstrap.js') }}"></script>
 
     {{-- Vite JS --}}
     {{-- {{ module_vite('build-home', 'resources/assets/js/app.js') }} --}}
