3368f99906c06595411fa75e204b1df94b1ff4cc
AeroBytes <kelvin@aerobytes.nl>
2025-03-11 13:01:23 +0100

    Added livewire files and migration

---
+++ Modules/Users/app/Livewire/UsersExport.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\Users\Livewire;
+
+use Livewire\Component;
+
+class UsersExport extends Component
+{
+    public function render()
+    {
+        return view('users::livewire.users-export');
+    }
+}
