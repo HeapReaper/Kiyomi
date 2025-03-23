b57f6f49ad2e126e72f9c5b60a108c1e9648c8cc
AeroBytes <kelvin@aerobytes.nl>
2025-03-23 18:35:03 +0100

    Fixed indentation in SettingsController.php

--- Modules/Settings/app/Http/Controllers/SettingsController.php
+++ Modules/Settings/app/Http/Controllers/SettingsController.php
@@ -27,13 +27,12 @@ class SettingsController extends Controller
     {
         $validated = $request->validate([
             'email_new_members' => ['email', 'nullable'],
-			'role_management_allowed_sign_in' => ['int', 'nullable'],
-			'role_junior_member_allowed_sign_in' => ['int', 'nullable'],
-			'role_aspirant_member_allowed_sign_in' => ['int', 'nullable'],
-			'role_member_allowed_sign_in' => ['int', 'nullable'],
-			'role_donor_allowed_sign_in' => ['int', 'nullable'],
-			'role_not_paid_allowed_sign_in' => ['int', 'nullable'],
-
+            'role_management_allowed_sign_in' => ['int', 'nullable'],
+            'role_junior_member_allowed_sign_in' => ['int', 'nullable'],
+            'role_aspirant_member_allowed_sign_in' => ['int', 'nullable'],
+            'role_member_allowed_sign_in' => ['int', 'nullable'],
+            'role_donor_allowed_sign_in' => ['int', 'nullable'],
+            'role_not_paid_allowed_sign_in' => ['int', 'nullable'],
         ]);
 
         Settings::insertOrUpdate('email_new_members', $validated['email_new_members'] ?? 0);
