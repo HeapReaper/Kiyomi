c36ccfeeff28053e2ffe3d63fb68001fd6d76600
AeroBytes <kelvin@aerobytes.nl>
2025-03-24 17:50:46 +0100

    Added login role check

--- Modules/Users/app/Livewire/Signin.php
+++ Modules/Users/app/Livewire/Signin.php
@@ -7,6 +7,7 @@ use Livewire\Attributes\Validate;
 use Livewire\Component;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
+use Modules\Users\Models\User;
 
 class Signin extends Component
 {
@@ -22,6 +23,22 @@ class Signin extends Component
         Log::channel('access')->warning('Failed login attempt. Email: ' . $this->email . ' IP: ' . request()->ip());
 			  return $this->addError('credentials', 'Email of wachtwoord is niet juist.');
 		}
+
+    $user = User::find(Auth::id());
+
+    $rolesAllowedLogin = array_map('trim', explode(',', \App\Helpers\Settings::get('roles_allowed_sign_in')));
+    $canSignIn = false;
+
+    foreach ($user->getRoleNames() as $role) {
+      if (in_array($role, $rolesAllowedLogin)) {
+          $canSignIn = true;
+      }
+    }
+
+    if (!$canSignIn) {
+        Log::channel('access')->warning('Someone tried to sign in while that user isnt allowed to. Email: ' . $this->email . ' IP: ' . request()->ip());
+        return $this->addError('credentials', 'Je mag niet inloggen!');
+    }
 
     Log::channel('access')->info('Login successful. Email: ' . $this->email . ' IP: ' . request()->ip());
 		return redirect()->route('flights-panel.index');
