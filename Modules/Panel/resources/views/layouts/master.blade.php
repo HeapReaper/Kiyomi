b707f2fd86614d3b895e4f12c2a5651b009a3113
AeroBytes <kelvin@aerobytes.nl>
2025-03-24 09:51:16 +0100

    Added navbar animations

--- Modules/Panel/resources/views/layouts/master.blade.php
+++ Modules/Panel/resources/views/layouts/master.blade.php
@@ -249,6 +249,33 @@
         .image-hover-resize-10:hover {
             transform: scale(1.2);
         }
+
+        .dropdown-toggle::after {
+            display: inline-block;
+            margin-left: 0.255em;
+            vertical-align: 0.255em;
+            content: "";
+            border-top: 0.3em solid;
+            border-right: 0.3em solid transparent;
+            border-bottom: 0;
+            border-left: 0.3em solid transparent;
+            transition: transform 0.6s ease-in-out;
+        }
+
+        .dropdown-toggle.show::after {
+            transform: rotate(180deg);
+        }
+
+        .dropdown-menu {
+            opacity: 0;
+            transition: opacity 0.1s ease-in-out;
+            display: block;
+        }
+
+        .dropdown-menu.show {
+            opacity: 1;
+        }
+
       </style>
 
       <livewire:scripts />
