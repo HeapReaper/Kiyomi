cdb7678d2d98caa952d86edd8971f48fba39be4c
AeroBytes <kelvin@aerobytes.nl>
2025-03-24 09:23:06 +0100

    Updated user show to support multiple roles

--- Modules/Users/resources/views/pages/show.blade.php
+++ Modules/Users/resources/views/pages/show.blade.php
@@ -5,59 +5,59 @@
 @section('content')
   <div class="ms-3 me-3 mt-0">
     <div class="container-fluid col-lg-6 offset-lg-3 pt-4 pb-4">
-      <div class="row bg-dark rounded bg-opacity-25">
+      <div class="row bg-dark rounded bg-opacity-25 shadow-lg">
         <h2 class="text-white font-weight-bold">Lid weergeven</h2>
 
         <div class="col-sm">
           <div class="pt-2 pb-2 pl-2 pr-2 mb-2 mt-2">
             <div class="form-group">
               <label for="name" class="text-white font-weight-bold"><strong>Volledige naam</strong></label>
-              <input type="text" class="form-control" id="name" name="name" aria-describedby="fullname" placeholder="Voornaam achternaam" required value="{{ $user->name }}" disabled>
+              <input type="text" class="form-control" id="name" name="name" aria-describedby="fullname" placeholder="Voornaam achternaam" value="{{ $user->name }}" required disabled>
               <!-- <small id="fullname" class="form-text text-muted"></small>-->
             </div>
 
             <div class="form-group">
               <label for="birthdate"  class="text-white font-weight-bold"><strong>Geboortedatum</strong></label>
-              <input type="text" class="form-control" id="birthdate" name="birthdate" placeholder="01-01-2024" required value="{{ date('d-m-Y', strtotime($user->birthdate)) }}" disabled>
+              <input type="text" class="form-control" id="birthdate" name="birthdate" placeholder="01-01-2024" value="{{ \Carbon\Carbon::createFromFormat('Y-m-d', $user->birthdate)->format('d-m-Y') }}" required disabled>
             </div>
           </div>
         </div>
 
         <div class="col-sm">
-          <div class="pt-2 pb-2 pl-2 pr-2 mb-2 mt-2 rounded">
+          <div class="pt-2 pb-2 pl-2 pr-2 mb-2 mt-2">
             <div class="form-group">
               <label for="address"  class="text-white font-weight-bold"><strong>Adres</strong></label>
-              <input type="text" class="form-control" id="address" name="address" placeholder="Straatnaam nummer" required value="{{ $user->address }}" disabled>
+              <input type="text" class="form-control" id="address" name="address" placeholder="Straatnaam nummer" value="{{ $user->address }}" required disabled>
             </div>
 
             <div class="form-group">
               <label for="postcode"  class="text-white font-weight-bold"><strong>Postcode</strong></label>
-              <input type="text" class="form-control" id="postcode" name="postcode" placeholder="1234AH (zonder spatie!)" required value="{{ $user->zip_code }}" disabled>
+              <input type="text" class="form-control" id="postcode" name="postcode" placeholder="1234AH (zonder spatie!)" value="{{ $user->zip_code }}" required disabled>
             </div>
 
             <div class="form-group">
               <label for="city"  class="text-white font-weight-bold"><strong>Woonplaats</strong></label>
-              <input type="text" class="form-control" id="city" name="city" placeholder="Woonplaats" required value="{{ $user->city }}" disabled>
+              <input type="text" class="form-control" id="city" name="city" placeholder="Woonplaats" value="{{ $user->city }}" required disabled>
             </div>
           </div>
         </div>
 
         <div class="col-sm">
-          <div class="pt-2 pb-2 pl-2 pr-2 mb-2 mt-2 rounded">
+          <div class="pt-2 pb-2 pl-2 pr-2 mb-2 mt-2">
             <div class="form-group">
               <label for="phone"  class="text-white font-weight-bold"><strong>Telefoon</strong></label>
-              <input type="text" class="form-control" id="phone" name="phone" placeholder="0612343455 (nummer zonder streepje!)" required value="{{ $user->mobile_phone }}" disabled>
+              <input type="text" class="form-control" id="phone" name="phone" placeholder="0612343455 (nummer zonder streepje!)" value="{{ $user->mobile_phone }}" required disabled>
             </div>
 
             <div class="form-group">
               <label for="email" class="text-white font-weight-bold"><strong>E-mail</strong></label>
-              <input type="email" class="form-control" id="email" name="email" placeholder="mail@provider.nl" required value="{{ $user->email }}" disabled>
+              <input type="email" class="form-control" id="email" name="email" placeholder="mail@provider.nl" value="{{ $user->email }}" required disabled>
             </div>
           </div>
         </div>
       </div>
 
-      <div class="row bg-dark rounded bg-opacity-25 mt-2">
+      <div class="row bg-dark rounded bg-opacity-25 shadow-lg mt-2">
         <div class="col-sm">
           <div class="pt-2 pb-2 pl-2 pr-2 mb-2 mt-2">
             <div class="form-group">
@@ -67,7 +67,19 @@
 
             <div class="form-group">
               <label for="knvvl"  class="text-white font-weight-bold"><strong>KNVvl nummer</strong></label>
-              <input type="text" class="form-control" id="knvvl" name="knvvl" placeholder="1234567" value="{{ $user->KNVvl }}" disabled>
+              <input type="text" class="form-control" id="knvvl" name="knvvl" placeholder="1234567" value="{{ $user->knvvl }}" disabled>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm">
+          <div class="pt-2 pb-2 pl-2 pr-2 mb-2 mt-2">
+            <div class="form-group">
+              <label for="instruct" class="text-white font-weight-bold"><strong>Instructeur</strong></label>
+              <select class="form-control" id="instruct" name="instruct" required disabled>
+                <option value=0 @selected($user->instruct === 0)>Nee</option>
+                <option value=1 @selected($user->instruct === 1)>Ja</option>
+              </select>
             </div>
           </div>
         </div>
@@ -76,126 +88,125 @@
           <div class="pt-2 pb-2 pl-2 pr-2 mb-2 mt-2">
             <div class="form-group">
               <label for="role" class="text-white font-weight-bold"><strong>Rol</strong></label>
-              <select class="form-control" id="role" name="role" required disabled>
-                <option value="junior_member" @if ($user->hasRole('junior_member')) selected @endif>
-                  Jeugd lid
-                </option>
-                <option value="aspirant_member" @if ($user->hasRole('aspirant_member')) selected @endif>
-                  Aspirant lid
-                </option>
-                <option value="member" @if ($user->hasRole('member')) selected @endif>
-                  Lid
-                </option>
-                <option value="management" @if ($user->hasRole('management')) selected @endif>
-                  Bestuur
-                </option>
-                <option value="donor" @if ($user->hasRole('donor')) selected @endif>
-                  Donateur
-                </option>
-                <option value="not_paid" @if ($user->hasRole('not_paid')) selected @endif>
-                  Niet betaald
-                </option>
-                <option value="" @if ($user->hasRole('super admin')) selected @endif>
-                  Super Admin
-                </option>
-              </select>
-            </div> 
 
-            <div class="form-group">
-              <label for="instruct" class="text-white font-weight-bold"><strong>Instructeur</strong></label>
-              <select class="form-control" id="instruct" name="instruct" required disabled>
-                <option value=0 @if ($user->instruct == 0) selected @endif>
-                  Nee
-                </option>
-                <option value=1 @if ($user->instruct == 1) selected @endif>
-                  Ja
-                </option>
-              </select>
-            </div> 
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="junior_member" id="junior_member" name="roles[]" disabled
+                        @checked($user->hasRole('junior_member') === true)>
+                <label class="form-check-label text-white" for="junior_member">
+                  Junior lid
+                </label>
+              </div>
+
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="aspirant_member" id="aspirant_member" name="roles[]" disabled
+                        @checked($user->hasRole('aspirant_member') === true)>
+
+                <label class="form-check-label text-white" for="aspirant_member">
+                  Aspirant lid
+                </label>
+              </div>
+
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="member" id="member" name="roles[]" disabled
+                        @checked($user->hasRole('member') === true)>
+                <label class="form-check-label text-white" for="member">
+                  Lid
+                </label>
+              </div>
+
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="management" id="management" name="roles[]" disabled
+                        @checked($user->hasRole('management') === true)>
+                <label class="form-check-label text-white" for="management">
+                  Bestuur
+                </label>
+              </div>
+
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="donor" id="donor" name="roles[]" disabled
+                        @checked($user->hasRole('donor') === true)>
+                <label class="form-check-label text-white" for="donor">
+                  Donateur
+                </label>
+              </div>
+            </div>
           </div>
         </div>
+      </div>
 
-        <div class="col-sm">
+      <div class="row bg-dark rounded bg-opacity-25 shadow-lg mt-2">
+        <div class="col">
           <div class="pt-2 pb-2 pl-2 pr-2 mb-2 mt-2">
-            <p class="font-weight-bold text-white mb-0"><strong>Brevetten</strong></p>
+            <div class="text-white font-weight-bold"><strong>Brevetten</strong></div>
             <div class="form-check">
-              <input class="form-check-input" type="checkbox" value=1 id="PlaneCertCheckbox" name="PlaneCertCheckbox"  @if ($user->has_plane_brevet == 1) checked @endif disabled>
+              <input class="form-check-input" type="checkbox" value=1 id="PlaneCertCheckbox" name="PlaneCertCheckbox" disabled
+                      @checked($user->has_plane_brevet === 1)>
               <label class="form-check-label text-white" for="PlaneCertCheckbox">
                 Motorvliegtuig
               </label>
             </div>
 
             <div class="form-check">
-              <input class="form-check-input" type="checkbox" value=1 id="HeliCertCheckbox" name="HeliCertCheckbox" @if ($user->has_helicopter_brevet == 1) checked @endif disabled>
+              <input class="form-check-input" type="checkbox" value=1 id="HeliCertCheckbox" name="HeliCertCheckbox" disabled
+                      @checked($user->has_helicopter_brevet === 1)>
               <label class="form-check-label text-white" for="HeliCertCheckbox">
                 Helicopter
               </label>
             </div>
 
             <div class="form-check">
-              <input class="form-check-input" type="checkbox" value=1 id="gliderCertCheckbox" name="gliderCertCheckbox" @if ($user->has_glider_brevet == 1) checked @endif disabled>
+              <input class="form-check-input" type="checkbox" value=1 id="gliderCertCheckbox" name="gliderCertCheckbox" disabled
+                      @checked($user->has_glider_brevet === 1)>
               <label class="form-check-label text-white" for="gliderCertCheckbox">
                 Zweefvliegtuig
               </label>
             </div>
+          </div>
+        </div>
 
-            <p class="font-weight-bold text-white mb-0 mt-3"><strong>Erelid</strong></p>
+        <div class="col">
+          <div class="pt-2 pb-2 ps-2 pe-2 mb-2 mt-2 w-25">
+            <div class="text-white font-weight-bold"><strong>Drone</strong></div>
             <div class="form-check">
-              <input class="form-check-input" type="checkbox" value=1 id="honoraryMemberCheckbox" name="honoraryMemberCheckbox" @if ($user->in_memoriam == 1) checked @endif disabled>
+              <input class="form-check-input" type="checkbox" value=1 id="droneA1Checkbox" name="droneA1Checkbox" disabled
+                      @checked($user->has_drone_a1 === 1)>
+              <label class="form-check-label text-white" for="droneA1Checkbox">
+                A1
+              </label>
+            </div>
+
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" value=1 id="droneA2Checkbox" name="droneA2Checkbox" disabled
+                      @checked($user->has_drone_a2 === 1)>
+              <label class="form-check-label text-white" for="droneA2Checkbox">
+                A2
+              </label>
+            </div>
+
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" value=1 id="droneA3Checkbox" name="droneA3Checkbox" disabled
+                      @checked($user->has_drone_a3 === 1)>
+              <label class="form-check-label text-white" for="droneA3Checkbox">
+                A3
+              </label>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm">
+          <div class="pt-2 pb-2 pl-2 pr-2 mb-2 mt-2">
+            <div class="text-white font-weight-bold"><strong>Status</strong></div>
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" value=1 id="honoraryMemberCheckbox" name="honoraryMemberCheckbox" disabled
+                      @checked($user->in_memoriam === 1)>
               <label class="form-check-label text-white" for="honoraryMemberCheckbox">
                 Erelid
               </label>
             </div>
           </div>
         </div>
-      </div>
 
-        <div class="row bg-dark rounded bg-opacity-25 mt-2">
-          <div class="col-sm">
-            <div class="pt-2 pb-2 ps-2 pe-2 mb-2 mt-2 w-25">
-              <h4 class="font-weight-bold text-white mb-0">Drone certificaten</h4>
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" value=1 id="droneA1Checkbox" name="droneA1Checkbox" @if ($user->has_drone_a1 == 1) checked @endif disabled>
-                <label class="form-check-label text-white" for="droneA1Checkbox">
-                  A1
-                </label>
-              </div>
 
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" value=1 id="droneA2Checkbox" name="droneA2Checkbox" @if ($user->has_drone_a2 == 1) checked @endif disabled>
-                <label class="form-check-label text-white" for="droneA2Checkbox">
-                  A2
-                </label>
-              </div>
-
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" value=1 id="droneA3Checkbox" name="droneA3Checkbox" @if ($user->has_drone_a3 == 1) checked @endif disabled>
-                <label class="form-check-label text-white" for="droneA3Checkbox">
-                  A3
-                </label>
-              </div>
-            </div>
-          </div>
-        </div>
-      
-       <div class="row bg-dark rounded bg-opacity-25 mt-2">
-        <div class="col-sm">
-          <div class="pt-2 pb-2 ps-2 pe-2 mb-2 mt-2 w-25">
-            <h4 class="font-weight-bold text-white mb-1">Betalingen</h4>
-            
-            <div class="row">
-              <div class="col">
-                <h5>Betalingen</h5>
-              </div>
-              
-              <div class="col">
-                <h5>Openstaande betalingen</h5>
-              </div>
-            </div>
-        </div>
-      </div>
-      
-      </div>
     </div>
   </div>
 @endsection
