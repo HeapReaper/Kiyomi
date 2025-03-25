a56f1cefe2facdf4c02e20c8f460813fecb17f64
AeroBytes <kelvin@aerobytes.nl>
2025-03-25 11:57:45 +0100

    Renamed licence pivot table

--- Modules/Users/database/migrations/2025_03_25_110850_license_user_pivot_migration.php
+++ Modules/Users/database/migrations/2025_03_25_110850_license_user_pivot_migration.php
@@ -8,7 +8,7 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('users_licences', function (Blueprint $table) {
+        Schema::create('license_user', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('pilot_licence_id');
@@ -18,6 +18,6 @@ return new class extends Migration
 
     public function down(): void
     {
-        Schema::dropIfExists('user_licence');
+        Schema::dropIfExists('license_user');
     }
 };
