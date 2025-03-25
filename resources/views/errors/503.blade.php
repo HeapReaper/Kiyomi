8ebeb801afe9c2ef0bae302fd09a62cde30d6d1a
AeroBytes <kelvin@aerobytes.nl>
2025-03-25 18:19:59 +0100

    Added maintenance mode

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,13 @@
+@extends('errors.layout')
+
+@section('title', '503')
+
+@section('content')
+  <div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
+    <div class="text-center">
+      <h1>Onderhoud</h1>
+      <img class="img-fluid rounded" src="/app_media/503-plane.webp" style="max-width: 300px">
+      <p>We zijn bezig met onderhoud aan deze website.</p>
+    </div>
+  </div>
+@stop
