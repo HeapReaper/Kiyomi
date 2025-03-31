3f9369b25191c2e86fdb64b611a6c6362549edac
AeroBytes <kelvin@aerobytes.nl>
2025-03-31 12:52:31 +0200

    Added basic home text

--- Modules/Home/resources/views/layouts/master.blade.php
+++ Modules/Home/resources/views/layouts/master.blade.php
@@ -42,17 +42,18 @@
 
     @include('home::includes.navbar')
 
-    <main>
+    <main class="m-2">
 
-      <div class="row">
-        <div class="col-8">
-          @yield('content')
+      <div class="row mt-3">
+        <div class="col-12 col-md-8">
+          <div class="ms-2">
+            @yield('content')
+          </div>
         </div>
 
-        <div class="col-4">
+        <div class="col-12 col-md-4">
           <div class="me-2">
             @include('home::includes.sidebar-search')
-
             @include('home::includes.sidebar-login')
           </div>
         </div>
