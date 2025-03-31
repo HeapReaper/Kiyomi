9b1daf7286b42089c1214dba22a30da67d562c61
AeroBytes <kelvin@aerobytes.nl>
2025-03-31 13:59:45 +0200

    Fixed Auth user bug

--- Modules/Home/resources/views/includes/sidebar-login.blade.php
+++ Modules/Home/resources/views/includes/sidebar-login.blade.php
@@ -1,22 +1,57 @@
 <div class="mt-5">
   <div class="sidebar-header">
     <div class="sidebar-title fw-bold">
-      Inloggen
+      @if (Auth::check())
+        Welkom {{ Auth::user()->name }}!
+      @else
+        Inloggen
+      @endif
     </div>
   </div>
 
   <form class="mt-4">
-    <div class="mb-3">
-      <label for="exampleInputEmail1" class="form-label text-black">Email</label>
-      <input type="email" class="form-control" id="email" name="email" placeholder="Email..." aria-describedby="">
-    </div>
+    @if (auth())
+      <div class="row">
+        <div class="col d-flex justify-content-center align-items-center">
+          <div class="mt-4 mb-3">
+            <img src="https://placehold.co/40x40" class="rounded">
+          </div>
+        </div>
 
-    <div class="mb-3">
-      <label for="password" class="form-label">Wachtwoord</label>
-      <input type="password" class="form-control" id="password" name="password" placeholder="Wachtwoord...">
-    </div>
+        <div class="col">
+          <div class="mt-4 mb-3">
+            <a href="#" class="sidebar-links">
+              Dashboard
+            </a>
+          </div>
 
-    <button type="submit" class="btn fw-bold button-subtle-animation" style="background-color: #d80414; color: white; width: 100px;">Log in</button>
+          <div class="mt-4 mb-3">
+            <a href="#" class="sidebar-links">
+              Profiel
+            </a>
+          </div>
+
+          <div class="mt-4 mb-3">
+            <a href="#" class="sidebar-links">
+              Uitloggen
+            </a>
+          </div>
+        </div>
+      </div>
+
+    @else
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label text-black">Email</label>
+        <input type="email" class="form-control" id="email" name="email" placeholder="Email..." aria-describedby="">
+      </div>
+
+      <div class="mb-3">
+        <label for="password" class="form-label">Wachtwoord</label>
+        <input type="password" class="form-control" id="password" name="password" placeholder="Wachtwoord...">
+      </div>
+
+      <button type="submit" class="btn fw-bold button-subtle-animation" style="background-color: #d80414; color: white; width: 100px;">Log in</button>
+    @endif
   </form>
 
 </div>
