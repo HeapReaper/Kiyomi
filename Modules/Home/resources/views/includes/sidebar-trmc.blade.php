c2002e5f42e3229a83f7428d748f065911cc5c48
AeroBytes <kelvin@aerobytes.nl>
2025-03-31 13:11:22 +0200

    Added sidebar TRMC

---
+++ Modules/Home/resources/views/includes/sidebar-trmc.blade.php
@@ -0,0 +1,19 @@
+<div class="mt-4">
+  <div class="sidebar-header">
+    <div class="sidebar-title fw-bold">
+      TRMC
+    </div>
+  </div>
+
+  <div class="mt-4 mb-3">
+    <a href="#">
+      Privacy verklaring
+    </a>
+  </div>
+
+  <div class="mt-4 mb-3">
+    <a href="#">
+      Vlucht aanmelden
+    </a>
+  </div>
+</div>
