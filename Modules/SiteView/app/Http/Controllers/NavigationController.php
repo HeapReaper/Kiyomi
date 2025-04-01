5070ce74166d30384eb81ed6ffd2af345c67c453
AeroBytes <kelvin@aerobytes.nl>
2025-04-01 13:46:30 +0200

    Removed default comments

--- Modules/SiteView/app/Http/Controllers/NavigationController.php
+++ Modules/SiteView/app/Http/Controllers/NavigationController.php
@@ -7,57 +7,36 @@ use Illuminate\Http\Request;
 
 class NavigationController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         return view('siteview::index');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return view('siteview::create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Show the specified resource.
-     */
     public function show($id)
     {
         return view('siteview::show');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit($id)
     {
         return view('siteview::edit');
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy($id)
     {
         //
