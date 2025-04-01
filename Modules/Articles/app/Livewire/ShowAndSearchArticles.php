075477f69a969fd24efba2bde74d6bdc0c9993bc
AeroBytes <kelvin@aerobytes.nl>
2025-04-01 18:37:46 +0200

    Added search field articles

---
+++ Modules/Articles/app/Livewire/ShowAndSearchArticles.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Modules\Articles\Livewire;
+
+use Livewire\Component;
+use Modules\Articles\Models\Article;
+
+class ShowAndSearchArticles extends Component
+{
+    public string $articleSearch = '';
+
+    public function render()
+    {
+        // Article::with('categories', 'author')->orderBy('created_at', 'desc')->get()
+        return view('articles::livewire.show-and-search-articles', [
+            'articles' => Article::query()
+                ->where(function ($query) {
+                    $query->where('id', 'like', '%' . $this->articleSearch . '%')
+                        ->orWhere('title', 'like', '%' . $this->articleSearch . '%')
+                        ->orWhereHas('author', function ($query) {
+                            $query->where('name', 'like', '%' . $this->articleSearch . '%');
+                        });
+                })
+                ->get()
+        ]);
+    }
+}
