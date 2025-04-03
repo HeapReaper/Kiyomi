cbbff044fd9c377ffada31fb2dfbf54d2ac81bd4
AeroBytes <kelvin@aerobytes.nl>
2025-04-03 10:40:08 +0200

    Kinda fixed table on mobile articles

--- Modules/Articles/resources/views/articles/index.blade.php
+++ Modules/Articles/resources/views/articles/index.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Alle artikelen')
 
 @section('content')
-  <div class="container mt-4 bg-dark rounded bg-opacity-25 shadow-lg">
+  <div class="container m-2 mt-4 bg-dark rounded bg-opacity-25 shadow-lg">
     <livewire:articles::show-and-search-articles />
     <br>
   </div>
--- Modules/Articles/resources/views/livewire/show-and-search-articles.blade.php
+++ Modules/Articles/resources/views/livewire/show-and-search-articles.blade.php
@@ -10,60 +10,63 @@
     </div>
   </div>
 
-  <table class="table table-custom-dark user-list ml-2 mr-2">
-    <thead>
-    <tr>
-      <th scope="col" class="text-white">#</th>
-      <th scope="col" class="text-white">Titel</th>
-      <th scope="col" class="text-white">Slug</th>
-      <th scope="col" class="text-white">Author</th>
-      <th scope="col" class="text-white">Datum en tijd</th>
-      <th scope="col" class="text-white">Gepubliceerd</th>
-      <th scope="col" class="text-white">Categorieën</th>
-      <th scope="col" class="text-white">Opties</th>
-    </tr>
-    </thead>
-
-    <tbody>
-    @foreach ($articles as $article)
+  <div class="table-responsive">
+    <table class="table table-custom-dark user-list ml-2 mr-2">
+      <thead>
       <tr>
-        <th scope="row" class="text-white">{{ $article->id }}</th>
-        <th scope="row" class="text-white">{{ $article->title }}</th>
-        <th scope="row" class="text-white">{{ $article->slug }}</th>
-        <th scope="row" class="text-white">{{ $article->author->name }}</th>
-        <th scope="row" class="text-white">
-          {{ \Carbon\Carbon::parse($article->created_at)->format('d-m-Y H:i:s') }}
-        </th>
-        <th scope="row" class="text-white">{{ $article->published === true ? 'Ja' : 'Nee' }}</th>
-        <th scope="row" class="text-white">
-          @foreach($article->categories as $category)
-            {{ $category->name }}<br>
-          @endforeach
-        </th>
-        <td style="width: 20%;" class="text-center">
-          <div style="display: flex;">
-            <form action="{{ route('articles.edit', $article->id) }}" method="GET" style="margin-right: 10px;">
-              @csrf
-              <button type="submit" class="table-link text-info image-hover-resize-10" style="border: none; background: none; padding: 0; cursor: pointer;">
-                <x-heroicon-o-pencil stroke="white" style="width: 27px;" />
-              </button>
-            </form>
-
-            <form action="{{ route('articles.destroy', $article->id) }}" method="POST">
-              @method('DELETE')
-              @csrf
-              <button type="submit" class="table-link text-info image-hover-resize-10"
-                      onclick="return confirm('Weet je zeker dat het artikel {{ $article->name }} wilt verwijderen?');"
-                      style="border: none; background: none; padding: 0; cursor: pointer;">
-                <x-heroicon-o-trash stroke="white" style="width: 27px;" />
-              </button>
-            </form>
-          </div>
-        </td>
+        <th scope="col" class="text-white">#</th>
+        <th scope="col" class="text-white">Titel</th>
+        <th scope="col" class="text-white">Slug</th>
+        <th scope="col" class="text-white">Author</th>
+        <th scope="col" class="text-white">Datum en tijd</th>
+        <th scope="col" class="text-white">Gepubliceerd</th>
+        <th scope="col" class="text-white">Categorieën</th>
+        <th scope="col" class="text-white">Opties</th>
       </tr>
-    @endforeach
-    </tbody>
-  </table>
+      </thead>
+
+      <tbody>
+      @foreach ($articles as $article)
+        <tr>
+          <th scope="row" class="text-white">{{ $article->id }}</th>
+          <th scope="row" class="text-white">{{ $article->title }}</th>
+          <th scope="row" class="text-white">{{ $article->slug }}</th>
+          <th scope="row" class="text-white">{{ $article->author->name }}</th>
+          <th scope="row" class="text-white">
+            {{ \Carbon\Carbon::parse($article->created_at)->format('d-m-Y H:i:s') }}
+          </th>
+          <th scope="row" class="text-white">{{ $article->published === true ? 'Ja' : 'Nee' }}</th>
+          <th scope="row" class="text-white">
+            @foreach($article->categories as $category)
+              {{ $category->name }}<br>
+            @endforeach
+          </th>
+          <td style="width: 20%;" class="text-center">
+            <div style="display: flex;">
+              <form action="{{ route('articles.edit', $article->id) }}" method="GET" style="margin-right: 10px;">
+                @csrf
+                <button type="submit" class="table-link text-info image-hover-resize-10" style="border: none; background: none; padding: 0; cursor: pointer;">
+                  <x-heroicon-o-pencil stroke="white" style="width: 27px;" />
+                </button>
+              </form>
+
+              <form action="{{ route('articles.destroy', $article->id) }}" method="POST">
+                @method('DELETE')
+                @csrf
+                <button type="submit" class="table-link text-info image-hover-resize-10"
+                        onclick="return confirm('Weet je zeker dat het artikel {{ $article->name }} wilt verwijderen?');"
+                        style="border: none; background: none; padding: 0; cursor: pointer;">
+                  <x-heroicon-o-trash stroke="white" style="width: 27px;" />
+                </button>
+              </form>
+            </div>
+          </td>
+        </tr>
+      @endforeach
+      </tbody>
+    </table>
+  </div>
+
 
   {!! $articles->links('pagination::bootstrap-5') !!}
 </div>
