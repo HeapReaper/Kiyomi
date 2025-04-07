e246ae8ac15e074643267a5b5169e16721c174c5
AeroBytesNL <kelvin@aerobytes.nl>
2025-04-07 10:48:29 +0200

    Moved toast HTML outside if statement home module

--- Modules/Home/resources/views/includes/notifications.blade.php
+++ Modules/Home/resources/views/includes/notifications.blade.php
@@ -1,63 +1,63 @@
 <!-- Errors -->
-@if ($errors->any())
-  <div class="toast-container showposition-fixed bottom-0 end-0 p-3">
-    <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-      <div class="toast-header">
-        <img src="/app_media/trmc.png" class="rounded me-2" alt="..." style="max-width: 35px">
-        <strong class="me-auto">Fout!</strong>
-        <small>Een paar seconden geleden</small>
-        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-      </div>
-      <div class="toast-body">
-        @foreach ($errors->all() as $error)
-          <li>{{ $error }}</li>
-        @endforeach
-      </div>
+<div class="toast-container showposition-fixed bottom-0 end-0 p-3">
+  <div id="liveToastMultipleErrors" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="toast-header">
+      <img src="/app_media/trmc.png" class="rounded me-2" alt="..." style="max-width: 35px">
+      <strong class="me-auto">Fout!</strong>
+      <small>Een paar seconden geleden</small>
+      <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+    </div>
+    <div class="toast-body">
+      @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+      @endforeach
     </div>
   </div>
+</div>
 
+@if ($errors->any())
   <script>
-    (new bootstrap.Toast(document.getElementById('liveToast'))).show()
+    (new bootstrap.Toast(document.getElementById('liveToastMultipleErrors'))).show()
   </script>
 @endif
 
-@if (session()->has('error'))
-  <div class="toast-container showposition-fixed bottom-0 end-0 p-3">
-    <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-      <div class="toast-header">
-        <img src="/app_media/trmc.png" class="rounded me-2" alt="..." style="max-width: 35px">
-        <strong class="me-auto">Fout!</strong>
-        <small>Een paar seconden geleden</small>
-        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-      </div>
-      <div class="toast-body">
-        {{ session('error') }}
-      </div>
+<!-- Single error -->
+<div class="toast-container showposition-fixed bottom-0 end-0 p-3">
+  <div id="liveToastError" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="toast-header">
+      <img src="/app_media/trmc.png" class="rounded me-2" alt="..." style="max-width: 35px">
+      <strong class="me-auto">Fout!</strong>
+      <small>Een paar seconden geleden</small>
+      <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+    </div>
+    <div class="toast-body">
+      {{ session('error') }}
     </div>
   </div>
+</div>
 
+@if (session()->has('error'))
   <script>
-    (new bootstrap.Toast(document.getElementById('liveToast'))).show()
+    (new bootstrap.Toast(document.getElementById('liveToastError'))).show()
   </script>
 @endif
 
 <!-- Success -->
-@if (session()->has('success'))
-  <div class="toast-container showposition-fixed bottom-0 end-0 p-3">
-    <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-      <div class="toast-header">
-        <img src="/app_media/trmc.png" class="rounded me-2" alt="..." style="max-width: 35px">
-        <strong class="me-auto">Success!</strong>
-        <small>Een paar seconden geleden</small>
-        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-      </div>
-      <div class="toast-body">
-        {{ session('success') }}
-      </div>
+<div class="toast-container showposition-fixed bottom-0 end-0 p-3">
+  <div id="liveToastSuccess" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="toast-header">
+      <img src="/app_media/trmc.png" class="rounded me-2" alt="..." style="max-width: 35px">
+      <strong class="me-auto">Success!</strong>
+      <small>Een paar seconden geleden</small>
+      <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+    </div>
+    <div class="toast-body">
+      {{ session('success') }}
     </div>
   </div>
-
+</div>
+@if (session()->has('success'))
   <script>
-    (new bootstrap.Toast(document.getElementById('liveToast'))).show()
+    (new bootstrap.Toast(document.getElementById('liveToastSuccess'))).show()
   </script>
 @endif
