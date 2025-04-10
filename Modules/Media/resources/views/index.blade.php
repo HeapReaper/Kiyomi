3fbf9ca813f13ee64e9b801ee06779623df75642
AeroBytesNL <kelvin@aerobytes.nl>
2025-04-10 11:22:34 +0200

    Added Media model and migration

--- Modules/Media/resources/views/index.blade.php
+++ Modules/Media/resources/views/index.blade.php
@@ -3,14 +3,39 @@
 @section('title', 'Media')
 
 @section('content')
-  @foreach($files as $file)
-    @switch($file)
-      @case(str_contains($file, 'png'))
-        PNG: {{ $file }}<br>
-        @break
-      @case(str_contains($file, 'pdf'))
-        PDF: {{ $file }}<br>
-        @break
-    @endswitch
-  @endforeach
+  <div class="container">
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Bestand</th>
+          <th scope="col">Auteur</th>
+          <th scope="col">Opties</th>
+        </tr>
+      </thead>
+      <tbody>
+      @foreach($files as $file)
+        <tr>
+          <th scope="row">{{ $loop->index }}</th>
+          @switch($file)
+            @case(str_contains($file, 'png'))
+              <img src="{{ Storage::disk('minio')->url($file) }}" class="w-25">
+              PNG: {{ $file }}<br>
+              URL: {{ Storage::disk('minio')->url($file) }}
+              @break
+            @case(str_contains($file, 'pdf'))
+              PDF: {{ $file }}<br>
+              URL: {{ Storage::disk('minio')->url($file) }}
+              @break
+          @endswitch
+        </tr>
+      @endforeach
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tbody>
+    </table>
+  </div>
+
 @stop
