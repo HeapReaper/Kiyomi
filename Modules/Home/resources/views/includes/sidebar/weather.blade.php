d839eed07a508c713f66bc249c1d9d53715cdb47
AeroBytesNL <kelvin@aerobytes.nl>
2025-04-15 11:11:09 +0200

    Moved sidebar to own directory

---
+++ Modules/Home/resources/views/includes/sidebar/weather.blade.php
@@ -0,0 +1,11 @@
+<div class="mt-5">
+  <div class="sidebar-header">
+    <div class="sidebar-title fw-bold">
+      Het weer op de club
+    </div>
+  </div>
+
+  <div class="mt-4 mb-3">
+
+  </div>
+</div>
--- Modules/Home/resources/views/layouts/master.blade.php
+++ Modules/Home/resources/views/layouts/master.blade.php
@@ -1,214 +1,215 @@
 <!DOCTYPE html>
 <html lang="nl">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <title>@yield('title')</title>
+  <title>@yield('title')</title>
 
-    <meta name="description" content="{{ $description ?? '' }}">
-    <meta name="keywords" content="{{ $keywords ?? '' }}">
-    <meta name="author" content="{{ $author ?? '' }}">
+  <meta name="description" content="{{ $description ?? '' }}">
+  <meta name="keywords" content="{{ $keywords ?? '' }}">
+  <meta name="author" content="{{ $author ?? '' }}">
 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
 
-    <link href="{{ asset('bootstrap-5.3.3-dist/css/bootstrap.css') }}" rel="stylesheet">
-    <script src="{{ asset('bootstrap-5.3.3-dist/js/bootstrap.bundle.js') }}"></script>
+  <link href="{{ asset('bootstrap-5.3.3-dist/css/bootstrap.css') }}" rel="stylesheet">
+  <script src="{{ asset('bootstrap-5.3.3-dist/js/bootstrap.bundle.js') }}"></script>
 
-    <link rel="icon" href="/app_media/faticon.ico" type="image/x-icon">
+  <link rel="icon" href="/app_media/faticon.ico" type="image/x-icon">
 
-    {{-- Vite CSS --}}
-    {{-- {{ module_vite('build-home', 'resources/assets/sass/app.scss') }} --}}
-    @vite(['Modules/Home/resources/assets/js/app.js', 'Modules/Home/resources/assets/css/app.css'])
-  </head>
+  {{-- Vite CSS --}}
+  {{-- {{ module_vite('build-home', 'resources/assets/sass/app.scss') }} --}}
+  @vite(['Modules/Home/resources/assets/js/app.js', 'Modules/Home/resources/assets/css/app.css'])
+</head>
 
-  <body>
-    <!-- Header image -->
-    <div class="position-relative">
-      <img src="/app_media/header-img.jpg" class="img-fluid w-100 header-img" alt="Overlay Image">
-      <div class="position-absolute start-50 translate-middle text-white text-center header-text" style="top: 25%">
-        <h1 class="fw-bold display-6">
-          Twentse Radio Modelvlieg Club
-        </h1>
-        <p class="mt-3">
-          Een prachtige hobby – Modelvliegen.
-        </p>
+<body>
+<!-- Header image -->
+<div class="position-relative">
+  <img src="/app_media/header-img.jpg" class="img-fluid w-100 header-img" alt="Overlay Image">
+  <div class="position-absolute start-50 translate-middle text-white text-center header-text" style="top: 25%">
+    <h1 class="fw-bold display-6">
+      Twentse Radio Modelvlieg Club
+    </h1>
+    <p class="mt-3">
+      Een prachtige hobby – Modelvliegen.
+    </p>
+  </div>
+</div>
+
+<!-- NAVBAR -->
+@include('home::includes.navbar')
+
+<!-- PAGE CONTENT -->
+<main class="m-2">
+  <div class="row mt-3">
+    <!-- MAIN TEXT -->
+    <div class="col-12 col-md-8">
+      <div class="ms-2">
+        @yield('content')
       </div>
     </div>
 
-    <!-- NAVBAR -->
-    @include('home::includes.navbar')
-
-    <!-- PAGE CONTENT -->
-    <main class="m-2">
-      <div class="row mt-3">
-        <!-- MAIN TEXT -->
-        <div class="col-12 col-md-8">
-          <div class="ms-2">
-            @yield('content')
-          </div>
-        </div>
-
-        <!-- SIDE BAR -->
-        <div class="col-12 col-md-4">
-          <div class="me-2">
-            @include('home::includes.sidebar-search')
-            @include('home::includes.sidebar-login')
-            @include('home::includes.sidebar-clubs')
-            @include('home::includes.sidebar-vacations')
-            @include('home::includes.sidebar-organisations')
-            @include('home::includes.sidebar-social-media')
-            @include('home::includes.sidebar-trmc')
-          </div>
-        </div>
+    <!-- SIDE BAR -->
+    <div class="col-12 col-md-4">
+      <div class="me-2">
+        @include('home::includes.sidebar.search')
+        @include('home::includes.sidebar.login')
+        @include('home::includes.sidebar.weather')
+        @include('home::includes.sidebar.clubs')
+        @include('home::includes.sidebar.vacations')
+        @include('home::includes.sidebar.trmc')
+        @include('home::includes.sidebar.organisations')
+        @include('home::includes.sidebar.social-media')
       </div>
+    </div>
+  </div>
 
-      <!-- NOTIFICATIONS -->
-      @include('home::includes.notifications')
-    </main>
-    <!-- FOOTER -->
-    @include('home::includes.footer')
+  <!-- NOTIFICATIONS -->
+  @include('home::includes.notifications')
+</main>
+<!-- FOOTER -->
+@include('home::includes.footer')
 
-    <style>
-      :root {
-        --primary-style-color: #d80414;
-      }
+<style>
+  :root {
+    --primary-style-color: #d80414;
+  }
 
-      body, html {
-        background-color: white;
-        width: 100%;
-        min-height: 100%;
-        background-attachment: fixed;
-        font-family: 'Open Sans', sans-serif;
-      }
+  body, html {
+    background-color: white;
+    width: 100%;
+    min-height: 100%;
+    background-attachment: fixed;
+    font-family: 'Open Sans', sans-serif;
+  }
 
-      .form-control:focus {
-        outline: 2px solid var(--primary-style-color);
-        box-shadow: 0 0 0 0.0rem rgba(0, 0, 0, 0);
-      }
+  .form-control:focus {
+    outline: 2px solid var(--primary-style-color);
+    box-shadow: 0 0 0 0.0rem rgba(0, 0, 0, 0);
+  }
 
-      .form-control:hover {
-        outline: 2px solid var(--primary-style-color);
-        box-shadow: 0 0 0 0.0rem rgba(0, 0, 0, 0);
-      }
+  .form-control:hover {
+    outline: 2px solid var(--primary-style-color);
+    box-shadow: 0 0 0 0.0rem rgba(0, 0, 0, 0);
+  }
 
-      input[type="checkbox"]:focus {
-          box-shadow: 0 0 0 0.2rem rgba(0, 128, 0, 0.25);
-      }
+  input[type="checkbox"]:focus {
+    box-shadow: 0 0 0 0.2rem rgba(0, 128, 0, 0.25);
+  }
 
-      .form-control option {
-          color: #ffffff;
-          padding: 8px 16px;
-          border: 1px solid transparent;
-          border-color: transparent transparent rgba(0, 0, 0, 0.1) transparent;
-          cursor: pointer;
-      }
+  .form-control option {
+    color: #ffffff;
+    padding: 8px 16px;
+    border: 1px solid transparent;
+    border-color: transparent transparent rgba(0, 0, 0, 0.1) transparent;
+    cursor: pointer;
+  }
 
-      .header-img {
-        object-fit: cover;
-      }
+  .header-img {
+    object-fit: cover;
+  }
 
-      @media (max-width: 768px) {
-        .header-text  {
-          margin-top: 20px;
-          top: 50%;
-          width: 90%;
-        }
+  @media (max-width: 768px) {
+    .header-text {
+      margin-top: 20px;
+      top: 50%;
+      width: 90%;
+    }
 
-        .header-img {
-          height: 200px;
-        }
-      }
+    .header-img {
+      height: 200px;
+    }
+  }
 
-      @media (max-width: 480px) {
-        .header-text {
-          top: 55%;
-          font-size: 16px;
-        }
-      }
+  @media (max-width: 480px) {
+    .header-text {
+      top: 55%;
+      font-size: 16px;
+    }
+  }
 
-      .navbar {
-        background-color: var(--primary-style-color) !important;
-      }
+  .navbar {
+    background-color: var(--primary-style-color) !important;
+  }
 
-      .sidebar-header {
-        padding: 10px;
-        position: relative;
-      }
+  .sidebar-header {
+    padding: 10px;
+    position: relative;
+  }
 
-      .sidebar-title {
-        background-color: var(--primary-style-color);
-        color: white;
-        padding: 4px 40px 4px 4px;
-        position: absolute;
-        top: 0;
-        left: 0;
-        clip-path: polygon(0 0, 80% 0, 100% 100%, 0 100%); /** angle shape **/
-      }
+  .sidebar-title {
+    background-color: var(--primary-style-color);
+    color: white;
+    padding: 4px 40px 4px 4px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    clip-path: polygon(0 0, 80% 0, 100% 100%, 0 100%); /** angle shape **/
+  }
 
-      .sidebar-links {
-        color: black;
-        text-decoration: underline var(--primary-style-color);
-        display: inline-block;
-        transition: transform 0.3s ease;
-      }
+  .sidebar-links {
+    color: black;
+    text-decoration: underline var(--primary-style-color);
+    display: inline-block;
+    transition: transform 0.3s ease;
+  }
 
-      .sidebar-links:hover {
-        transform: translateX(5px);
-      }
+  .sidebar-links:hover {
+    transform: translateX(5px);
+  }
 
-      .button-subtle-animation {
-        transition: transform 0.2s ease-in-out;
-      }
+  .button-subtle-animation {
+    transition: transform 0.2s ease-in-out;
+  }
 
-      .button-subtle-animation:hover {
-        transform: scale(1.05);
-      }
+  .button-subtle-animation:hover {
+    transform: scale(1.05);
+  }
 
-      footer {
-        background-color: var(--primary-style-color);
-        padding: 0 !important;
-        margin: 0 !important;
-      }
+  footer {
+    background-color: var(--primary-style-color);
+    padding: 0 !important;
+    margin: 0 !important;
+  }
 
-      .dropdown-menu .dropdown-item:hover {
-        background-color: rgba(0,0,0,0.2);
-      }
+  .dropdown-menu .dropdown-item:hover {
+    background-color: rgba(0, 0, 0, 0.2);
+  }
 
-      .dropdown-toggle::after {
-        display: inline-block;
-        margin-left: 0.255em;
-        vertical-align: 0.255em;
-        content: "";
-        border-top: 0.3em solid;
-        border-right: 0.3em solid transparent;
-        border-bottom: 0;
-        border-left: 0.3em solid transparent;
-        transition: transform 0.6s ease-in-out;
-      }
+  .dropdown-toggle::after {
+    display: inline-block;
+    margin-left: 0.255em;
+    vertical-align: 0.255em;
+    content: "";
+    border-top: 0.3em solid;
+    border-right: 0.3em solid transparent;
+    border-bottom: 0;
+    border-left: 0.3em solid transparent;
+    transition: transform 0.6s ease-in-out;
+  }
 
-      .dropdown-toggle.show::after {
-        transform: rotate(180deg);
-      }
+  .dropdown-toggle.show::after {
+    transform: rotate(180deg);
+  }
 
-      .dropdown-menu {
-        opacity: 0;
-        transition: opacity 0.1s ease-in-out;
-        display: block;
-        pointer-events: none;
-      }
+  .dropdown-menu {
+    opacity: 0;
+    transition: opacity 0.1s ease-in-out;
+    display: block;
+    pointer-events: none;
+  }
 
-      .dropdown-menu.show {
-        opacity: 1;
-        pointer-events: auto;
-      }
-    </style>
+  .dropdown-menu.show {
+    opacity: 1;
+    pointer-events: auto;
+  }
+</style>
 
-    {{-- Vite JS --}}
-    {{-- {{ module_vite('build-home', 'resources/assets/js/app.js') }} --}}
-  </body>
+{{-- Vite JS --}}
+{{-- {{ module_vite('build-home', 'resources/assets/js/app.js') }} --}}
+</body>
 </html>
