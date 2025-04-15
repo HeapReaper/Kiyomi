e5bda34cad5ee1bd3737daac37c75098b1ad1e73
AeroBytesNL <kelvin@aerobytes.nl>
2025-04-15 13:17:48 +0200

    Added icon and directions for arrow

--- Modules/Home/resources/views/includes/sidebar/weather.blade.php
+++ Modules/Home/resources/views/includes/sidebar/weather.blade.php
@@ -22,24 +22,41 @@
         $weather = $response->json();
 
         $windDirectionDegrees = [
-          'N'  => 0,
-          'NE' => 45,
-          'E'  => 90,
-          'SE' => 135,
-          'S'  => 180,
-          'SW' => 225,
-          'W'  => 270,
-          'NW' => 315,
+            'N'   => 0,
+            'NNE' => 22.5,
+            'NE'  => 45,
+            'ENE' => 67.5,
+            'E'   => 90,
+            'ESE' => 112.5,
+            'SE'  => 135,
+            'SSE' => 157.5,
+            'S'   => 180,
+            'SSW' => 202.5,
+            'SW'  => 225,
+            'WSW' => 247.5,
+            'W'   => 270,
+            'WNW' => 292.5,
+            'NW'  => 315,
+            'NNW' => 337.5,
         ];
     @endphp
 
-    <p>
-      Locatie: {{ $weather['location']['name'] }}<br>
-      Temperatuur: {{ $weather['current']['temp_c'] }}C<br>
-      Wind: {{ $weather['current']['wind_kph'] }}km/u<br>
-      Windstoten: {{ $weather['current']['gust_kph'] }}km/u<br>
-      Windrichting: {{ $weather['current']['wind_dir'] }}<br>
-    </p>
+    <div class="row">
+      <div class="col">
+        <p>
+          Locatie: {{ $weather['location']['name'] }}<br>
+          Temperatuur: {{ $weather['current']['temp_c'] }}C<br>
+          Wind: {{ $weather['current']['wind_kph'] }}km/u<br>
+          Windstoten: {{ $weather['current']['gust_kph'] }}km/u<br>
+          Windrichting: {{ $weather['current']['wind_dir'] }}<br>
+        </p>
+      </div>
+
+      <div class="col d-flex justify-content-center ">
+        <img src="{{ $weather['current']['condition']['icon'] }}" style="width: 90px; height: 90px">
+      </div>
+    </div>
+
 
     <div class="position-relative d-inline-block" style="height: 100px;">
       <img src="/app_media/field-top.png" class="img rounded" style="height: 100px;">
