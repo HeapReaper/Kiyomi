e59fc4c9df61c3b470bfd256a4e76e293674d34b
AeroBytesNL <kelvin@aerobytes.nl>
2025-04-26 15:26:16 +0200

    Added middleware Payments module

--- Modules/Payments/routes/web.php
+++ Modules/Payments/routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Payments\Http\Controllers\PaymentsController;
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::group(['middleware' => ['auth', 'role:management,webmaster']], function () {
     Route::resource('payments', PaymentsController::class)->names('payments');
 });
