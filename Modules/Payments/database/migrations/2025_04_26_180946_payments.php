a09f78100256a49403e2dd4883fffa70315fc3c8
AeroBytesNL <kelvin@aerobytes.nl>
2025-04-26 20:23:18 +0200

    Added migration payments

---
+++ Modules/Payments/database/migrations/2025_04_26_180946_payments.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->integer('type');
+            $table->integer('period');
+            $table->date('starts_at')->nullable();
+            $table->date('ends_at')->nullable();
+            $table->decimal('amount', 8, 2);
+            $table->integer('status')->default(0);
+            $table->integer('payment_method')->nullable();
+            $table->string('stripe_payment_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
