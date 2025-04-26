7d4326a97fcc01b83663150c6682d7730c5137dc
AeroBytesNL <kelvin@aerobytes.nl>
2025-04-26 15:25:41 +0200

    Added middleware Memberships module

--- Modules/Memberships/routes/web.php
+++ Modules/Memberships/routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Memberships\Http\Controllers\MembershipsController;
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::group(['middleware' => ['auth', 'role:management,webmaster']], function () {
     Route::resource('memberships', MembershipsController::class)->names('memberships');
 });
