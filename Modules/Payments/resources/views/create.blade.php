7832c64139a8aef4243dd7b066f744b1dc3d193b
AeroBytesNL <kelvin@aerobytes.nl>
2025-04-30 11:21:17 +0200

    Added basic new payment frontend

--- Modules/Payments/resources/views/create.blade.php
+++ Modules/Payments/resources/views/create.blade.php
@@ -2,9 +2,43 @@
 
 @section('title', 'Betaling nieuw')
 
-
 @section('content')
-  <h1>Hello World</h1>
+  <div class="container bg-dark bg-opacity-25 rounded shadow-lg mt-2 p-2">
+    <h2 class="text-white">Nieuwe betaling</h2>
 
-  <p>Module: {!! config('payments.name') !!}</p>
-@endsection
+    <form action="{{ route('payments.store') }}" method="POST">
+      <div class="form-group mb-3">
+        <label for="method" class="text-white font-weight-bold"><strong>Betalingsmethode</strong></label>
+        <select class="form-control" id="method" name="method" required>
+          <option value=1 selected>
+            Contant
+          </option>
+          <option value=2>
+            Bankoverschrijving
+          </option>
+          <option value=3>
+            iDeal
+          </option>
+        </select>
+      </div>
+
+      <div class="form-group mb-3">
+        <label for="type" class="text-white font-weight-bold"><strong>Type</strong></label>
+        <select class="form-control" id="type" name="type" required>
+          <option value=1 selected>
+            Lidmaatschap
+          </option>
+          <option value=2>
+            Donatie
+          </option>
+          <option value=3>
+            Subsidie
+          </option>
+          <option value=4>
+            Overig
+          </option>
+        </select>
+      </div>
+    </form>
+  </div>
+@stop
