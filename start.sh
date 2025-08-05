6ba366124df7de68e6bc18be7c8d8135d4aa7441
AeroBytesNL <kelvin@aerobytes.nl>
2025-08-05 16:29:34 +0200

    Added extra sleep

--- start.sh
+++ start.sh
@@ -6,6 +6,8 @@ sleep 3
 
 curl -X PUT --data-binary @/docker-entrypoint.d/config.json --unix-socket /var/run/control.unit.sock http://localhost/config
 
-php /var/www/html/artisan view:clear
+sleep 3
+
+php artisan view:clear
 
 tail -f /dev/null
