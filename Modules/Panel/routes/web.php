9fe9e3119016a4745a51a39169dd0084f38a61d6
AeroBytesNL <kelvin@aerobytes.nl>
2025-08-05 19:48:35 +0200

    Added test job

--- Modules/Panel/routes/web.php
+++ Modules/Panel/routes/web.php
@@ -3,9 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Panel\Http\Controllers\NotificationController;
 use Modules\Panel\Http\Controllers\PanelController;
+use App\Jobs\TestJob;
 
 Route::group(['middleware' => 'auth'], function () {
     Route::resource('panel', PanelController::class)->names('panel');
     Route::post('/notifications/read', [NotificationController::class, 'markSingleAsRead'])->name('notifications.markReadSingle');
 });
 
+Route::get('/dispatch-test', function () {
+    TestJob::dispatch();
+    return 'Job dispatched!';
+});
