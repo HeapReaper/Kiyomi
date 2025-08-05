0ef4a71bb8992f216e10fb5791d8598610111461
AeroBytesNL <kelvin@aerobytes.nl>
2025-08-05 16:23:13 +0200

    Added php artisan view:clear

--- start.sh
+++ start.sh
@@ -6,4 +6,6 @@ sleep 3
 
 curl -X PUT --data-binary @/docker-entrypoint.d/config.json --unix-socket /var/run/control.unit.sock http://localhost/config
 
+php /var/www/html/artisan view:clear
+
 tail -f /dev/null
