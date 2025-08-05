b92a25583f29f2ba01185f7404b1fc963caf2a9b
AeroBytesNL <kelvin@aerobytes.nl>
2025-08-05 16:34:31 +0200

    Fix

--- start.sh
+++ start.sh
@@ -4,10 +4,8 @@ supervisord -c /etc/supervisor/conf.d/supervisord.conf &
 
 sleep 3
 
-curl -X PUT --data-binary @/docker-entrypoint.d/config.json --unix-socket /var/run/control.unit.sock http://localhost/config
-
-sleep 3
-
 php artisan view:clear
+
+curl -X PUT --data-binary @/docker-entrypoint.d/config.json --unix-socket /var/run/control.unit.sock http://localhost/config
 
 tail -f /dev/null
