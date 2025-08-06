e1317928f8da9750d0f89a8641829e93439222dd
AeroBytesNL <kelvin@aerobytes.nl>
2025-08-06 12:52:36 +0200

    Added responsecache

--- Modules/Flights/routes/web.php
+++ Modules/Flights/routes/web.php
@@ -5,14 +5,15 @@ use Modules\Flights\Http\Controllers\FlightsController;
 use Modules\Flights\Http\Controllers\FlightsPanelController;
 use Modules\Flights\Http\Controllers\FlightsReportController;
 use Modules\Flights\Http\Controllers\FlightsStatisticsController;
+use Spatie\ResponseCache\Middlewares\CacheResponse;
 
-Route::group([], function () {
+Route::middleware(CacheResponse::class)->group(function () {
     Route::resource('flights', FlightsController::class)->names('flights');
-	  Route::get('/flights', [FlightsController::class, 'index']);
+    Route::get('/flights', [FlightsController::class, 'index']);
     Route::get('/aanmeld-formulier', [FlightsController::class, 'redirect']);
 });
 
-Route::group(['middleware' => 'auth'], function () {
+Route::middleware(['auth', CacheResponse::class])->group(function () {
     Route::resource('flights-panel', FlightsPanelController::class)->names('flights-panel');
     Route::resource('flights-reports', FlightsReportController::class)->names('flights-report');
     Route::get('flights-reports/download/{report}', [FlightsReportController::class, 'download']);
