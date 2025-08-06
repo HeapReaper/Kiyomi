54e7cc89b44cb291e9fcc2737173165459c059c7
AeroBytesNL <kelvin@aerobytes.nl>
2025-08-06 13:04:04 +0200

    Added auth to /pulse

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Pulse\Pulse;
+use Modules\Users\Models\User;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('viewPulse', function (User $user) {
+            return $user->hasRole('webmaster');
+        });
     }
 }
