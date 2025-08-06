11f17000b6aedd644e7d98f4bbec2afc18890778
AeroBytesNL <kelvin@aerobytes.nl>
2025-08-06 14:55:17 +0200

    Added env value to pulse

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Gate::define('viewPulse', function (User $user) {
-            return auth()->check();
+            return env('ENABLE_VIEW_PULSE', false);
         });
     }
 }
