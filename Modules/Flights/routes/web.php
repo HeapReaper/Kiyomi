e6c469f1f535cda4b78485bdd684f9d67b578743
AeroBytesNL <kelvin@aerobytes.nl>
2025-08-06 19:37:42 +0200

    updated Pulse

--- Modules/Flights/routes/web.php
+++ Modules/Flights/routes/web.php
@@ -7,13 +7,13 @@ use Modules\Flights\Http\Controllers\FlightsReportController;
 use Modules\Flights\Http\Controllers\FlightsStatisticsController;
 use Spatie\ResponseCache\Middlewares\CacheResponse;
 
-Route::middleware(CacheResponse::class)->group(function () {
+Route::group([], function () {
     Route::resource('flights', FlightsController::class)->names('flights');
     Route::get('/flights', [FlightsController::class, 'index']);
     Route::get('/aanmeld-formulier', [FlightsController::class, 'redirect']);
 });
 
-Route::middleware(['auth', CacheResponse::class])->group(function () {
+Route::middleware(['auth'])->group(function () {
     Route::resource('flights-panel', FlightsPanelController::class)->names('flights-panel');
     Route::resource('flights-reports', FlightsReportController::class)->names('flights-report');
     Route::get('flights-reports/download/{report}', [FlightsReportController::class, 'download']);
