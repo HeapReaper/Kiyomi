2ac00c5560492e6bf6598a5f6002e3bc903598e8
AeroBytesNL <kelvin@aerobytes.nl>
2025-08-17 18:31:28 +0200

    Fixed statistics not showing on first page load

--- Modules/Flights/resources/views/livewire/statistics.blade.php
+++ Modules/Flights/resources/views/livewire/statistics.blade.php
@@ -1,4 +1,4 @@
-<div class=" bg-dark bg-opacity-25 rounded m-2 p-2">
+<div class="bg-dark bg-opacity-25 rounded m-2 p-2">
   <script
     src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js">
   </script>
@@ -27,121 +27,88 @@
   </div>
 
   <script>
-    const options = {
-      responsive: true,
-      maintainAspectRatio: true,
-      legend: {
-        labels: {
-          fontColor: '#FFFFFF'
-        }
-      },
-      scales: {
-        xAxes: [{
-          ticks: {
-            fontColor: '#FFFFFF'
-          }
-        }],
-        yAxes: [{
-          ticks: {
-            fontColor: '#FFFFFF'
-          }
-        }]
-      },
-      tooltips: {
-        bodyFontColor: '#FFFFFF',
-        titleFontColor: '#FFFFFF'
-      }
-    };
+      document.addEventListener("turbo:load", function () {
+          const options = {
+              responsive: true,
+              maintainAspectRatio: true,
+              legend: {
+                  labels: {
+                      fontColor: '#FFFFFF'
+                  }
+              },
+              scales: {
+                  xAxes: [{
+                      ticks: { fontColor: '#FFFFFF' }
+                  }],
+                  yAxes: [{
+                      ticks: { fontColor: '#FFFFFF' }
+                  }]
+              },
+              tooltips: {
+                  bodyFontColor: '#FFFFFF',
+                  titleFontColor: '#FFFFFF'
+              }
+          };
 
-    const months = [
-      "Januari",
-      "Februari",
-      "Maart",
-      "April",
-      "Mei",
-      "Juni",
-      "Juli",
-      "Augustus",
-      "September",
-      "Oktober",
-      "November",
-      "December"
-    ];
+          const months = [
+              "Januari", "Februari", "Maart", "April", "Mei",
+              "Juni", "Juli", "Augustus", "September",
+              "Oktober", "November", "December"
+          ];
 
-    const barColors= [
-      "red",
-      "green",
-      "blue",
-      "orange",
-      "brown",
-      "purple",
-      "cyan",
-      "magenta",
-      "lime",
-      "teal",
-      "pink",
-      "gold"
-    ];
+          const barColors= [
+              "red","green","blue","orange","brown",
+              "purple","cyan","magenta","lime","teal","pink","gold"
+          ];
 
-    const models = [
-      "Vliegtuig",
-      "Motorzweefvliegtuig",
-      "Zweefvliegtuig",
-      "Helikopter",
-      "Drone",
-    ];
+          const models = ["Vliegtuig","Motorzweefvliegtuig","Zweefvliegtuig","Helikopter","Drone"];
 
-    const flightsEachYear = @json($flightsEachYear);
-    const flightsEachMonthData = @json($flightsThisYearCount);
-    const top10PilotsData = @json($topTenPilots);
-    const modelsFlownData = @json($modelFlightsCount);
+          const flightsEachYear = @json($flightsEachYear);
+          const flightsEachMonthData = @json($flightsThisYearCount);
+          const top10PilotsData = @json($topTenPilots);
+          const modelsFlownData = @json($modelFlightsCount);
 
-    new Chart("flightsEachYear", {
-        type: "bar",
-        data: {
-            labels: Object.keys(flightsEachYear),
-            datasets: [{
-                backgroundColor: barColors,
-                data: Object.values(flightsEachYear)
-            }]
-        },
-        options: options
-    });
+          new Chart("flightsEachYear", {
+              type: "bar",
+              data: {
+                  labels: Object.keys(flightsEachYear),
+                  datasets: [{ backgroundColor: barColors, data: Object.values(flightsEachYear) }]
+              },
+              options: options
+          });
 
-    new Chart("flightsEachMonth", {
-      type: "bar",
-      data: {
-        labels: months,
-        datasets: [{
-          backgroundColor: barColors,
-          data: months.map(month => flightsEachMonthData[month] ?? 0)
-        }]
-      },
-      options: options
-    });
+          new Chart("flightsEachMonth", {
+              type: "bar",
+              data: {
+                  labels: months,
+                  datasets: [{
+                      backgroundColor: barColors,
+                      data: months.map(month => flightsEachMonthData[month] ?? 0)
+                  }]
+              },
+              options: options
+          });
 
-    new Chart("top10PilotsWithFlights", {
-      type: "bar",
-      data: {
-        labels: Object.keys(top10PilotsData),
-        datasets: [{
-          backgroundColor: barColors,
-          data: Object.values(top10PilotsData)
-        }]
-      },
-      options: options
-    });
+          new Chart("top10PilotsWithFlights", {
+              type: "bar",
+              data: {
+                  labels: Object.keys(top10PilotsData),
+                  datasets: [{ backgroundColor: barColors, data: Object.values(top10PilotsData) }]
+              },
+              options: options
+          });
 
-    new Chart("modelsFlown", {
-      type: "pie",
-      data: {
-        labels: models,
-        datasets: [{
-          backgroundColor: barColors,
-          data: models.map(model => modelsFlownData[model] ?? 0)
-        }]
-      },
-      options: options
-    });
+          new Chart("modelsFlown", {
+              type: "pie",
+              data: {
+                  labels: models,
+                  datasets: [{
+                      backgroundColor: barColors,
+                      data: models.map(model => modelsFlownData[model] ?? 0)
+                  }]
+              },
+              options: options
+          });
+      });
   </script>
 </div>
